fae20389a35eb361f817927f6c75a09b017c9bd5
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2008-11-28 12:43:00 +0000

    Version 0.2.2

---
+++ tags/wordmobi/0.2.2/src/settings.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import e32
+from appuifw import *
+import types
+            
+class Settings(object):
+    def __init__(self,
+                 cbk,
+                 blog_url= u"http://blogname.wordpress.com",                 
+                 username = u"",
+                 password = u"",             
+                 num_posts = 10,
+                 num_comments = 20):
+
+        self.cbk = cbk
+        self.username = username
+        self.password = password
+        self.blog_url = blog_url
+        self.num_posts = num_posts
+        self.num_comments = num_comments
+        
+        self.body = Listbox( [ (u"",u"") ], self.update_value )
+        self.cancel = False
+        self.last_idx = 0
+
+    def refresh(self):
+        app.exit_key_handler = self.close_app
+        app.title = u"Settings"
+
+        values = [ (u"Blog URL:", self.blog_url ), \
+                   (u"Username:", self.username ), \
+                   (u"Password:", u"*"*len( self.password )), \
+                   (u"Number of Posts:", unicode( self.num_posts )),
+                   (u"Number of Comments:", unicode( self.num_comments ))]
+
+        app.body = self.body
+        app.body.set_list( values, self.last_idx )
+        app.menu = [( u"Cancel", self.cancel_app )]        
+        
+    def cancel_app(self):
+        self.cancel = True
+        self.close_app()
+        
+    def close_app(self):
+        if not self.cancel:
+            self.cbk( ( self.blog_url, self.username, self.password, self.num_posts, self.num_comments) )
+        else:
+            self.cbk( (None,) )
+
+    def update_value(self):
+        idx = app.body.current()
+        self.last_idx = idx
+        
+        vars = ( "blog_url", "username", "password", "num_posts", "num_comments" )
+        labels = ( u"Blog URL:", u"Username:", u"Password:", u"Number of posts:", u"Number of comments" )
+        formats = ( "text", "text", "code", "number", "number" )
+        
+        val = query(labels[idx], formats[idx], self.__getattribute__(vars[idx]))
+        if val is not None:
+            if type(val) in ( types.UnicodeType , types.StringType ):
+                val = val.strip()
+            self.__setattr__( vars[idx],val )
+  
+        self.refresh()
+        
+    def run(self):
+        self.refresh()
+
+if __name__ == "__main__":
+
+    lock = e32.Ao_lock()
+    
+    def cbk( x ):
+        print x
+        lock.signal()
+        
+    dlg = Settings( cbk )
+    dlg.run()
+    
+    lock.wait()
