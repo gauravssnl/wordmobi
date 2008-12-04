b6ff64cdeafc13a7642b80aa9f9a3767d8fd6508
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2008-12-04 03:29:06 +0000

    initial support for comments

--- wordmobi/src/comments.py
+++ wordmobi/src/comments.py
@@ -3,17 +3,28 @@ import e32
 from appuifw import *
 from wmutil import *
 import wordpresslib as wp
+from xmlrpclib import DateTime
+import time
 
 class CommentContents(object):
+
+    PARAGRAPH_SEPARATOR = u"\u2029"
+
     def __init__(self, cbk, contents=u""):
         self.cbk = cbk
         self.cancel = False
 
-        self.body = Text( contents )
+        self.body = Text( self.commment_to_text(contents) )
         self.body.focus = True
         self.body.set_pos( 0 )
 
         self.refresh()
+
+    def commment_to_text(self,msg):
+        return msg.replace(u"\n",CommentContents.PARAGRAPH_SEPARATOR)
+    
+    def text_to_commment(self,msg):
+        return msg.replace(CommentContents.PARAGRAPH_SEPARATOR,u"\n")
     
     def refresh(self):            
         app.menu = [( u"Cancel", self.cancel_app )]        
@@ -29,7 +40,7 @@ class CommentContents(object):
         
     def close_app(self):
         if not self.cancel:
-            self.cbk( self.body.get() )
+            self.cbk( self.text_to_commment(self.body.get()) )
         else:
             self.cbk( None )
             
@@ -39,13 +50,15 @@ class CommentContents(object):
 class NewComment(object):
     def __init__(self,
                  cbk,
-                 name=u"",
+                 post_id,
+                 realname=u"",
                  email=u"",
                  website=u"http://",                 
                  contents=u""):
         
         self.cbk = cbk
-        self.name = name
+        self.post_id = post_id
+        self.realname = realname
         self.email = email
         self.website = website
         self.contents = contents
@@ -61,10 +74,10 @@ class NewComment(object):
         app.exit_key_handler = self.close_app
         app.title = self.app_title
 
-        self.lst_values = [ (u"Name", self.name ), \
+        self.lst_values = [ (u"Contents", self.contents[:50]), \
+                            (u"Name", self.realname ), \
                             (u"Email", self.email), \
-                            (u"Website", self.website), \
-                            (u"Contents", self.contents[:50]) ]
+                            (u"Website", self.website) ]
 
         app.body = self.body
         app.body.set_list( self.lst_values, self.last_idx )
@@ -92,17 +105,25 @@ class NewComment(object):
         if not self.cancel:
             ny = popup_menu( [u"Yes", u"No"], u"Send comment ?")
             if ny == 0:            
-                if self.cbk( (self.name, self.email, self.website, self.contents) ) == False:
+                if self.cbk( (self.post_id, self.email, self.realname, self.website, self.contents) ) == False:
                     self.refresh()
             else:
                 self.cbk( None )                    
         else:
-            self.cbk( None )
+            self.cbk( None )     
         
+    def update_contents(self):
+        def cbk( txt ):
+            if txt is not None:
+                self.contents = txt
+            self.refresh()
+        self.dlg = CommentContents( cbk, self.contents )
+        self.dlg.run()
+
     def update_name(self):
-        name = query(u"Name:","text", self.name)
-        if name is not None:
-            self.name = name
+        realname = query(u"Name:","text", self.realname)
+        if realname is not None:
+            self.realname = realname
         self.refresh()
 
     def update_email(self):
@@ -115,53 +136,78 @@ class NewComment(object):
         website = query(u"Website:","text", self.website)
         if website is not None:
             self.website = website
-        self.refresh()        
+        self.refresh()
         
-    def update_contents(self):
-        def cbk( txt ):
-            if txt is not None:
-                self.contents = txt
-            self.refresh()
-        self.dlg = CommentContents( cbk, self.contents )
-        self.dlg.run()
-
     def update_value_check_lock(self):
         if self.ui_is_locked() == False:
             self.update( app.body.current() )
             
     def update(self,idx):
         self.last_idx = idx
-        updates = ( self.update_name, self.update_email, \
-                    self.update_website, self.update_contents )
+        updates = ( self.update_contents, self.update_name, self.update_email, self.update_website )
         if idx < len(updates):
             updates[idx]()
 
     def run(self):
         self.refresh()
-    
+
+class EditComment(NewComment):
+    def __init__(self,
+                 cbk,
+                 idx,
+                 realname=u"",
+                 email=u"",
+                 website=u"http://",                 
+                 contents=u""):
+
+        self.idx = idx
+        super(EditComment,self).__init__(cbk, 0, realname, email, website, contents)
+
+        self.app_title = u"Edit comment"
+        
+    def close_app(self):
+        if not self.cancel:
+            ny = popup_menu( [u"No", u"Yes"], u"Update comment ?")
+            if ny == 1:            
+                if self.cbk( (self.idx, self.email, self.realname, self.website, self.contents) ) == False:
+                    self.refresh()
+            else:
+                self.cbk( None )                    
+        else:
+            self.cbk( None )  
+        
 class Comments(object):
     def __init__(self,
                  cbk,
                  blog,
                  comments,
-                 app_title=u"Comments"):
+                 app_title,
+                 email,
+                 realname,
+                 website):
 
         self.cbk = cbk
         self.blog = blog
         self.comments = comments
+        self.email = email
+        self.realname = realname
+        self.website = website
         self.app_title = app_title        
         self.ui_lock = False
         
         self.body = Listbox( [ (u"",u"") ], self.update_value_check_lock )
         self.last_idx = 0
-        self.menu = [ ( u"New", self.comment_reply ), \
-                      ( u"Update list", self.comment_update ), \
-                      ( u"Close", self.close_app ) ]
+        self.menu = [ ( u"Close", self.close_app ) ]
 
     def refresh(self):
         app.exit_key_handler = self.close_app
         app.title = self.app_title
 
+        if len(self.comments) == 0:
+            note(u"No comments for this post.","info")
+            self.close_app()
+            return
+            
         self.lst_values = []
         for comment in self.comments:
             (y, mo, d, h, m, s) = parse_iso8601( comment['date_created_gmt'].value )
@@ -192,23 +238,85 @@ class Comments(object):
         return self.ui_lock
         
     def close_app(self):
-        # TODO: need to update post info if any comment is changed here !!!!
-        # !!!!!!!!!!!!!
-        self.cbk()
+        self.cbk( )
 
-    def comment_update(self):
-        pass
+    def comment_replay_cbk(self, params):
+        if params is not None:
 
-    def comment_reply(self,parent=0):
-        def cbk( txt ):
-            if txt is not None:
-                pass
-            self.refresh()
-        self.dlg = NewComment( cbk )
+            (post_id, self.email, self.realname, self.website, self.contents) = params
+            
+            self.lock_ui( u"Sending comment %s" % self.contents[:15] )
+            
+            comment = wp.WordPressNewComment()
+            comment.status = 'approve'
+            comment.content = unicode_to_utf8( self.contents )
+            comment.author = unicode_to_utf8( self.realname )
+            comment.author_url = unicode_to_utf8( self.website )
+            comment.author_email = unicode_to_utf8( self.email )
+
+            try:
+                comment_id = self.blog.newComment(post_id, comment)
+            except:
+                note(u"Impossible to send the comment. Try again.","error")
+                self.unlock_ui()
+                return False
+            
+            try:
+                self.comments.insert( 0, self.blog.getComment( comment_id ) )
+            except:
+                note(u"Impossible to update the comment list. Try again.","error")
+                
+            self.unlock_ui()
+
+        self.refresh()
+        return True
+    
+    def comment_reply(self,post_id):
+        self.dlg = NewComment( self.comment_replay_cbk, post_id, self.realname, self.email, self.website, u"")
         self.dlg.run() 
+
+    def comment_view_cbk(self,params):
+        if params is not None:
+
+            (idx, self.email, self.realname, self.website, self.contents) = params
+            
+            self.lock_ui( u"Sending comment %s" % self.contents[:15] )
+
+            comment_id = self.comments[idx]['comment_id']
+            comment = wp.WordPressEditComment()
+            comment.status = 'approve'
+            comment.content = unicode_to_utf8( self.contents )
+            comment.author = unicode_to_utf8( self.realname )
+            comment.author_url = unicode_to_utf8( self.website )
+            comment.author_email = unicode_to_utf8( self.email )
+            comment.date_created_gmt = DateTime( time.mktime(time.gmtime()) ) # gmt time required
+        
+            try:
+                self.blog.editComment(comment_id,comment)
+            except:
+                note(u"Impossible to update the comment. Try again.","error")
+                self.unlock_ui()
+                return False
+
+                self.comments[idx]['content'] = comment.content
+                self.comments[idx]['author'] = comment.author
+                self.comments[idx]['author_url'] = comment.author_url
+                self.comments[idx]['author_email'] = comment.author_email
+                self.comments[idx]['content'] = comment.content
+                self.comments[idx]['date_created_gmt'] = comment.date_created_gmt
+                
+            self.unlock_ui()
+
+        self.refresh()
+        return True
     
     def comment_view(self,idx):
-        pass    
+        self.dlg = EditComment( self.comment_view_cbk, idx, \
+                                utf8_to_unicode( self.comments[idx]['author'] ), \
+                                utf8_to_unicode( self.comments[idx]['author_email'] ), \
+                                utf8_to_unicode( self.comments[idx]['author_url'] ), \
+                                utf8_to_unicode( self.comments[idx]['content'] ))
+        self.dlg.run() 
 
     def comment_delete(self,idx):
         ny = popup_menu( [u"No", u"Yes"], u"Delete comment ?")
@@ -216,7 +324,7 @@ class Comments(object):
             self.lock_ui(u"Deleting comment %s" % utf8_to_unicode( self.comments[idx]['content'][:15] ))
 
             try:
-                self.blog.deleteComment(self.comments[idx]['comment_id'])
+                self.blog.deleteComment( self.comments[idx]['comment_id'] )
             except:
                 note(u"Impossible to delete the comment. Try again.","error")
                 self.unlock_ui()
@@ -265,7 +373,7 @@ class Comments(object):
             if op == 0:
                 self.comment_view(idx)
             elif op == 1:
-                self.comment_reply(self.comments[idx]['parent'])
+                self.comment_reply(idx)
             elif op == 2:
                 self.comment_delete(idx)
             elif op == 3:
