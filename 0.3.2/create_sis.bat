1dcd0760d68471207b11465efb9151a51b34b7d0
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2008-12-08 12:54:46 +0000

    vesion 0.3.2

---
+++ 0.3.2/create_sis.bat
@@ -0,0 +1,27 @@
+@echo on
+
+IF "%1" EQU "" GOTO error
+
+SET PYTHON=C:\Python25\python
+SET APPNAME=Wordmobi
+SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
+SET SRCDIR=src
+SET TXTFILE=README
+SET TMPDIR=src.tmp
+SET ICON=wordmobi.svg
+
+IF NOT EXIST %TMPDIR% mkdir %TMPDIR%
+
+copy  %SRCDIR%\*.py  %TMPDIR%
+
+copy %SRCDIR%\wordmobi.py %TMPDIR%\default.py
+
+%PYTHON% .\ensymble.py py2sis --uid=0xefefefef --version="%1" --appname="%APPNAME%" --textfile="%TXTFILE%" --icon="%ICON%" --caps="%CAPBLS%" "%TMPDIR%" "%APPNAME%-%1.sis"
+
+goto end
+
+:error
+echo "Sintaxe: " $0 " version"
+
+:end
+
