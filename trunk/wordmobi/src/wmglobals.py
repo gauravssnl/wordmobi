9cb376c588c97442e27cf17a1abf1eb5b32dfe96
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-01-13 21:47:21 +0000

    Updating version number 0.4.2

--- trunk/wordmobi/src/wmglobals.py
+++ trunk/wordmobi/src/wmglobals.py
@@ -3,7 +3,7 @@
 __all__ = [ "VERSION", "DEFDIR", "PROMO_PHRASE", "MIFFILE" ]
 
 # always 3 numbers with two digits each maximum, e.g. 3.44.2, 4.2.33 ...
-VERSION = "0.4.1" 
+VERSION = "0.4.2" 
 PROMO_PHRASE = "<br><br>Posted by <a href=\"http://wordmobi.googlecode.com\">Wordmobi</a>"
 DEFDIR = "e:\\wordmobi\\"
 MIFFILE = "wordmobi.mif"
