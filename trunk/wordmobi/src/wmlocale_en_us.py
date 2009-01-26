40a3b8ef4e357c4c4095dc1659639594b072ef5a
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-01-26 03:04:28 +0000

    Adding localization support

---
+++ trunk/wordmobi/src/wmlocale_en_us.py
@@ -0,0 +1,112 @@
+# -*- coding: cp1252 -*-
+
+# Wordmobi main menu
+wm_menu_post = u"Posts"
+wm_menu_comm = u"Comments"
+wm_menu_tags = u"Tags"
+wm_menu_cats = u"Categories"
+wm_menu_sets = u"Settings"
+wm_menu_upgr = u"Upgrade"
+wm_menu_abou = u"About"
+wm_menu_exit = u"Exit"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Not supported yet."
+wm_err_no_proxy = u"Proxy authentication not supported for this feature."
+wm_err_upd_page = u"Impossible to access update page %s"
+wm_err_downld_fail = u"Impossible to download %s"
+wm_err_upd_info = u"Upgrade information missing."
+wm_err_cache_cleanup = u"Not all files in %s could be removed. Try to remove them later."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Checking update page..."
+wm_info_ver_is_updt = u"Your version is already updated."
+wm_info_downloading = u"Downloading ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s downloaded in %supdates. Please, install it."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Download %s ?"
+wm_pmenu_exit = u"Exit ?"
+
+# General messages
+gm_yes = u"Yes"
+gm_no = u"No"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"A Wordpress client"
+ab_menu_wordmobi_auth = u"Author"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Source code"
+ab_menu_wordmobi_blog = u"Blog"
+ab_menu_wordmobi_lics = u"License"
+ab_menu_wordmobi_warn = u"Warning"
+ab_menu_wordmobi_warn_val = u"Use at your own risk"
+
+# Categories main menu
+ca_menu_updt = u"Update"
+ca_menu_dele = u"Delete"
+ca_menu_cnew = u"Create new"
+ca_menu_clos = u"Close"
+
+# Categories popup menu
+ca_pmenu_cats = u"Categories:"
+ca_pmenu_delete = u"Delete %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Categories"
+ca_info_downld_cats = u"Downloading categories..."
+ca_info_del_cat = u"Deleting category %s ..."
+ca_info_create_cat = u"Creating category %s ..."
+
+# categories queries
+ca_query_cat_name = u"Category name:"
+
+# Comments main menu
+cm_menu_updt = u"Update"
+cm_menu_view = u"View/Edit"
+cm_menu_dele = u"Delete"
+cm_menu_cnew = u"Create new/Reply"
+cm_menu_clos = u"Close"
+cm_menu_canc = u"Cancel"
+cm_menu_pstt = u"Post title"
+cm_menu_cont = u"Contents"
+cm_menu_name = u"Name"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Website"
+        
+# Comments lists
+cm_list_any = u"Any"
+cm_list_spam = u"Spam"
+cm_list_moderated = u"Moderated"
+cm_list_unmoderated = u"Unmoderated"
+cm_list_one_post = u"One post"
+cm_list_all_posts = u"All posts"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Comments"
+cm_info_aprv_cmt = u"Approving comment %s"
+cm_info_downld_pt = u"Downloading post titles..."
+cm_info_which_post = u"For which post?"
+cm_info_downld_cmts = u"[%d/%d] Downloading comments ..."
+cm_info_downld_cmt = u"Downloading comments ..."
+cm_info_no_cmts_st = u"No comments with status '%s'."
+cm_info_udt_cmts_lst = u"Please, update the comment list."
+cm_info_del_cmt = u"Deleting comment %s"
+cm_info_upld_cmt = u"Uploading comment ..."
+cm_info_updt_cmt = u"Updating comment ..."
+cm_info_undef = u"Undefined"
+cm_info_empty = u"<empty>"
+cm_info_cmt_contents = u"Comment Contents"
+cm_info_new_cmt = u"New Comment"
+cm_info_edit_cmt = u"Edit Comment"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Comment status:"
+cm_pmenu_updt_for = u"Upd comments for?"
+cm_pmenu_downld_fail = u"Downl. failed ! Continue?"
+cm_pmenu_del_cmt = u"Delete comment ?"
+cm_pmenu_send_cmt = u"Send comment ?"
+cm_pmenu_updt_cmt = u"Update comment ?"
+cm_pmenu_comts = u"Comments:"
+
---
+++ trunk/wordmobi/src/wmlocale_pt_br.py
@@ -0,0 +1,111 @@
+# -*- coding: cp1252 -*-
+
+# Wordmobi main menu 
+wm_menu_post = u"Posts"
+wm_menu_comm = u"Comentários"
+wm_menu_tags = u"Tags"
+wm_menu_cats = u"Categorias"
+wm_menu_sets = u"Configurações"
+wm_menu_upgr = u"Atualização"
+wm_menu_abou = u"Sobre"
+wm_menu_exit = u"Sair"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Ainda não suportado."
+wm_err_no_proxy = u"Não existe ainda suporte a proxy para esta operação."
+wm_err_upd_page = u"Impossível acesssar a página de update %s"
+wm_err_downld_fail = u"Impossível baixar %s"
+wm_err_upd_info = u"Faltando informação sobre a atualização."
+wm_err_cache_cleanup = u"Nem todos os arquivos em %s puderam ser removidos. Tente removê-los mais tarde."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Verificando página de updates..."
+wm_info_ver_is_updt = u"Sua versão já está atualizada."
+wm_info_downloading = u"Baixando ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s salvo em %supdates. Por favor, instale-a."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Baixar %s ?"
+wm_pmenu_exit = u"Sair ?"
+
+# General messages
+gm_yes = u"Sim"
+gm_no = u"Não"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"Um cliente para Wordpress"
+ab_menu_wordmobi_auth = u"Autor"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Código fonte"
+ab_menu_wordmobi_blog = u"Blog"
+ab_menu_wordmobi_lics = u"Licença"
+ab_menu_wordmobi_warn = u"Advertência"
+ab_menu_wordmobi_warn_val = u"Assuma todos os riscos ao usar"
+
+# Categories main menu
+ca_menu_updt = u"Atualizar"
+ca_menu_dele = u"Apagar"
+ca_menu_cnew = u"Criar nova"
+ca_menu_clos = u"Fechar"
+
+# Categories popup menu
+ca_pmenu_cats = u"Categorias:"
+ca_pmenu_delete = u"Apagar %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Categorias"
+ca_info_downld_cats = u"Baixando categorias..."
+ca_info_del_cat = u"Apagando categoria %s ..."
+ca_info_create_cat = u"Criando categoria %s ..."
+
+# categories queries
+ca_query_cat_name = u"Nome da categoria:"
+
+# Comments main menu
+cm_menu_updt = u"Atualizar"
+cm_menu_view = u"Ver/Editar"
+cm_menu_dele = u"Apagar"
+cm_menu_cnew = u"Criar novo/Responder"
+cm_menu_clos = u"Fechar"
+cm_menu_canc = u"Cancel"
+cm_menu_pstt = u"Título do post"
+cm_menu_cont = u"Conteúdo"
+cm_menu_name = u"Nome"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Website"
+
+# Comments lists
+cm_list_any = u"Qualquer"
+cm_list_spam = u"Spam"
+cm_list_moderated = u"Moderado"
+cm_list_unmoderated = u"Não moderado"
+cm_list_one_post = u"Um post"
+cm_list_all_posts = u"Todos os posts"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Comentários"
+cm_info_aprv_cmt = u"Aprovando comentário %s"
+cm_info_downld_pt = u"Baixando títulos dos posts..."
+cm_info_which_post = u"Para qual post?"
+cm_info_downld_cmts = u"[%d/%d] Baixando comentários ..."
+cm_info_downld_cmt = u"Baixando comentários ..."
+cm_info_no_cmts_st = u"Sem comentários com status '%s'."
+cm_info_udt_cmts_lst = u"Por favor, atualize a lista de comentários."
+cm_info_del_cmt = u"Apagando comentário %s"
+cm_info_upld_cmt = u"Enviando comentário ..."
+cm_info_updt_cmt = u"Atualizando comentário ..."
+cm_info_undef = u"Indefinido"
+cm_info_empty = u"<vazio>"
+cm_info_cmt_contents = u"Conteúdo do comentário"
+cm_info_new_cmt = u"Novo Comentário"
+cm_info_edit_cmt = u"Editar Comentário"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Status do comentário:"
+cm_pmenu_updt_for = u"Atualiz. coment. para?"
+cm_pmenu_downld_fail = u"Downl. falhou ! Continuar?"
+cm_pmenu_del_cmt = u"Apagar comentário ?"
+cm_pmenu_send_cmt = u"Enviar comentário ?"
+cm_pmenu_updt_cmt = u"Atualizar comentário ?"
+cm_pmenu_comts = u"Comentários:"
