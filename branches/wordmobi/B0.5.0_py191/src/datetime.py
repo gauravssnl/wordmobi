d0eafac53ae5b9b2ff581a25c57494684103ac3e
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-02-19 12:04:36 +0000

    e32calendar replacing calendar

--- branches/wordmobi/B0.5.0_py191/src/datetime.py
+++ branches/wordmobi/B0.5.0_py191/src/datetime.py
@@ -21,7 +21,11 @@ XXX calendar.weekday may break for certain old times.. not sure why, but that is
 """
 
 import time as t
-import calendar
+import sys
+if float(sys.version[:3]) >= 1.9:
+    import e32calender as calendar
+else:
+    import calendar
 from types import InstanceType
     
 MINYEAR = 1
