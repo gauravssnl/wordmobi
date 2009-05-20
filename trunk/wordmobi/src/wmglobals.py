62ae75a352e1012ed6b164617b7a825a5704d7dd
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-05-20 03:17:14 +0000

    - Adding support for touch UI

--- trunk/wordmobi/src/wmglobals.py
+++ trunk/wordmobi/src/wmglobals.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__all__ = [ "VERSION", "DEFDIR", "MIFFILE", "PERSIST" ]
+__all__ = [ "VERSION", "DEFDIR", "MIFFILE", "PERSIST", "TOUCH_ENABLED" ]
 
 # always 3 numbers with two digits each maximum, e.g. 3.44.2, 4.2.33 ...
-VERSION = "0.7.2" 
+VERSION = "0.8.0-RC1" 
 import e32
 if e32.in_emulator():
     import sys
@@ -15,3 +15,11 @@ else:
     DEFDIR = "e:\\wordmobi\\"
 MIFFILE = "wordmobi2.mif"
 PERSIST = "persist.bin"
+
+try:
+    from appuifw import touch_enabled
+    TOUCH_ENABLED = touch_enabled()
+except:
+    # python < 1.9.3 does not support touch ui
+    TOUCH_ENABLED = False
+    
