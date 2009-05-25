420dacaef4073874781885c31a1ebbdec43215af
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-05-25 13:06:38 +0000

    Fixing sis creation with 1.9.4

--- trunk/wordmobi/create_sis.bat
+++ trunk/wordmobi/create_sis.bat
@@ -9,7 +9,11 @@ SET SRCDIR=src
 SET TXTFILE=README
 SET TMPDIR=src.tmp
 SET ICON=wordmobi.svg
+REM put you zip tool here
 SET ZIP="C:\Arquivos de programas\7-Zip\7z.exe"
+REM Path to module-repo, inside Python For S60 
+SET PYS60DIR=D:\S60\python1.9.4
+
 
 SET OPTS=--verbose --uid=0xefefefef --version="%1" ^
          --appname="%APPNAME%" --textfile="%TXTFILE%" --icon="%ICON%" ^
@@ -22,11 +26,13 @@ copy  %SRCDIR%\*.py  %TMPDIR%
 
 %PYTHON% ensymble.py py2sis %OPTS% --heapsize=4k,2M "%APPNAME%-%1.sis"
 
-echo "Generating for Python 1.9.2..."
+echo "Generating for Python 1.9.4..."
 rmdir /s /q %TMPDIR%
 mkdir %TMPDIR%\extras\data\python
 copy  %SRCDIR%\*.py  %TMPDIR%\extras\data\python
 move %TMPDIR%\extras\data\python\default.py  %TMPDIR%\
+if not exist .\module-repo\ xcopy /E "%PYS60DIR%\module-repo" .\module-repo\
+if not exist .\templates\   xcopy /E "%PYS60DIR%\templates"   .\templates\
 
 %PYTHON% ensymble_py19.py py2sis %OPTS% --extrasdir=extras --heapsize=4k,5M "%APPNAME%-%1-py19.sis"
 
