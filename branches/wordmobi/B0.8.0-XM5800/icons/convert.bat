fa7211a8b38d774ad07b45df415d1858f8cec602
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-05-25 14:15:43 +0000

    New version with tags support for XM 5800

---
+++ branches/wordmobi/B0.8.0-XM5800/icons/convert.bat
@@ -0,0 +1,5 @@
+.\bin\mifconv.exe wordmobi.mif /c32 about44.svg /c32 tags44.svg  /c32 categories44.svg /c32 comments44.svg /c32 posts44.svg /c32 setup44.svg /c32 update44.svg /c32 wpaccount44.svg /S.\bin\ /Hwordmobi.mbg /X
+pause
+
+
+
---
+++ branches/wordmobi/B0.8.0-XM5800/src/wmlocale_en_us.py
@@ -0,0 +1,325 @@
+# -*- coding: cp1252 -*-
+
+# Translation by:
+#
+# Marcelo Barros <marcelobarrosalmeida (at) gmail.com>
+
+# Promo phrase
+promo_phrase = u"<br><br>Posted by <a href=\"http://wordmobi.googlecode.com\">Wordmobi</a>" 
+
+# Wordmobi main menu
+wm_menu_post = u"Posts"
+wm_menu_comm = u"Comments"
+wm_menu_cats = u"Categories"
+wm_menu_tags = u"Tags"
+wm_menu_sets = u"Settings"
+wm_menu_upgr = u"Upgrade"
+wm_menu_abou = u"About"
+wm_menu_exit = u"Exit"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Not supported yet."
+wm_err_upd_page = u"Impossible to access update page %s"
+wm_err_downld_fail = u"Impossible to download %s"
+wm_err_upd_info = u"Upgrade information missing."
+wm_err_cache_cleanup = u"Not all files in %s could be removed. Try to remove them later."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Checking update page..."
+wm_info_ver_is_updt = u"Your version is already updated."
+wm_info_downloading = u"Downloading ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s downloaded in %supdates. Please, install it."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Download %s ?"
+wm_pmenu_exit = u"Exit ?"
+
+# General messages
+gm_yes = u"Yes"
+gm_no = u"No"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"A Wordpress client"
+ab_menu_wordmobi_auth = u"Author"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Source code"
+ab_menu_wordmobi_blog = u"Blog"
+ab_menu_wordmobi_lics = u"License"
+ab_menu_wordmobi_warn = u"Warning"
+ab_menu_wordmobi_warn_val = u"Use at your own risk"
+
+# Categories main menu
+ca_menu_updt = u"Update"
+ca_menu_dele = u"Delete"
+ca_menu_cnew = u"Create new"
+ca_menu_clos = u"Close"
+
+# Categories popup menu
+ca_pmenu_cats = u"Categories:"
+ca_pmenu_delete = u"Delete %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Categories"
+ca_info_downld_cats = u"Downloading categories..."
+ca_info_del_cat = u"Deleting category %s ..."
+ca_info_create_cat = u"Creating category %s ..."
+
+# categories queries
+ca_query_cat_name = u"Category name:"
+
+# Comments main menu
+cm_menu_updt = u"Update"
+cm_menu_view = u"View/Edit"
+cm_menu_dele = u"Delete"
+cm_menu_cnew = u"Create new/Reply"
+cm_menu_clos = u"Close"
+cm_menu_canc = u"Cancel"
+cm_menu_pstt = u"Post title"
+cm_menu_cont = u"Contents"
+cm_menu_name = u"Name"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Website"
+
+# Comments lists
+cm_list_any = u"Any"
+cm_list_spam = u"Spam"
+cm_list_moderated = u"Moderated"
+cm_list_unmoderated = u"Unmoderated"
+cm_list_approve = u"Approve"
+cm_list_one_post = u"One post"
+cm_list_all_posts = u"All posts"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Comments"
+cm_info_aprv_cmt = u"Approving comment %s"
+cm_info_downld_pt = u"Downloading post titles..."
+cm_info_which_post = u"For which post?"
+cm_info_downld_cmts = u"[%d/%d] Downloading comments ..."
+cm_info_downld_cmt = u"Downloading comments ..."
+cm_info_no_cmts_st = u"No comments with status '%s'."
+cm_info_udt_cmts_lst = u"Please, update the comment list."
+cm_info_del_cmt = u"Deleting comment %s"
+cm_info_upld_cmt = u"Uploading comment ..."
+cm_info_updt_cmt = u"Updating comment ..."
+cm_info_undef = u"Undefined"
+cm_info_empty = u"<empty>"
+cm_info_cmt_contents = u"Comment Contents"
+cm_info_new_cmt = u"New Comment"
+cm_info_edit_cmt = u"Edit Comment"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Comment status:"
+cm_pmenu_updt_for = u"Upd comments for?"
+cm_pmenu_downld_fail = u"Downl. failed ! Continue?"
+cm_pmenu_del_cmt = u"Delete comment ?"
+cm_pmenu_send_cmt = u"Send comment ?"
+cm_pmenu_updt_cmt = u"Update comment ?"
+cm_pmenu_comts = u"Comments:"
+
+# Posts main menu
+pt_menu_updt = u"Update"
+pt_menu_view = u"View/Edit"
+pt_menu_dele = u"Delete"
+pt_menu_lstc = u"List Comments"
+pt_menu_cnew = u"Create new"
+pt_menu_offl_publ = u"Publish"
+pt_menu_clos = u"Close"
+pt_menu_pict = u"Take a photo"
+pt_menu_canc = u"Cancel"
+pt_menu_text = u"Text"
+pt_menu_refs = u"Links/images"
+pt_menu_lsts = u"Lists"
+pt_menu_revs = u"Revision"
+pt_menu_prvw = u"Preview"
+pt_menu_titl = u"Title"
+pt_menu_cont = u"Contents"
+pt_menu_cats = u"Categories"
+pt_menu_tags = u"Tags"
+pt_menu_imgs = u"Images"
+pt_menu_pubs = u"Publish"
+pt_menu_s2tw = u"Send to Twitter"
+        
+# Posts lists
+pt_list_320x240 = u"(320x240)"
+pt_list_640x480 = u"(640x480)"
+pt_list_fsh_auto = u"Auto"
+pt_list_fsh_none = u"None"
+pt_list_fsh_forc = u"Forced"
+pt_list_loc_file = u"Local file"
+pt_list_url = u"URL"
+pt_list_ins_img = u"Insert"
+pt_list_take_img = u"Take a photo"
+pt_list_view_img = u"View/List"
+pt_list_rem_img = u"Remove"
+pt_list_save_it = u"No, but save it"
+pt_list_yes_rem_pst = u"Yes, just remote post"
+pt_list_yes_loc_ch = u"Yes, just local changes"
+pt_list_yes_del_all = u"Yes, delete all"
+pt_list_new_tag = u"Create"
+pt_list_sel_tag = u"Select"
+pt_list_rem_tag = u"Remove"
+pt_list_lst_tag = u"List"
+pt_list_sel_cat = u"Select"
+pt_list_rem_cat = u"Remove"
+pt_list_lst_cat = u"List"
+
+# Posts info
+pt_info_pst_pos = u"[%d/%d] Posts"
+pt_info_downld_pt = u"Downloading post titles..."
+pt_info_no_posts = u"No posts available."
+pt_info_del_post = u"Deleting post..."
+pt_info_post_del = u"Post deleted."
+pt_info_updt_pst_lst = u"Please, update the post list."
+pt_info_downld_post = u"Downloading post..."
+pt_info_empty = u"<empty>"
+pt_info_pict = u"Take a Photo"
+pt_info_post_contents = u"Post Contents"
+pt_info_new_post = u"New Post"
+pt_info_edit_post = u"Edit Post"
+pt_info_no_pub = u"Yes"
+pt_info_draft = u"No (draft)"
+pt_info_no_imgs_sel = u"No images selected."
+pt_info_downld_img = u"Downloading %s"
+pt_info_send_twt = u"Sending post title to Twitter"
+pt_info_twitter_updated = u"Twitter updated."
+
+# Posts error messages
+pt_err_cant_del_pt = u"Impossible to delete post."
+pt_err_cant_start_viewf = u"Could not start the view finder."
+pt_err_cant_stop_viewf = u"Could not stop the view finder."
+pt_err_cant_take_pic = u"Could not take any photo."
+pt_err_cant_gen_prvw = u"Could not generate preview file."
+pt_err_cant_prvw = u"Impossible to preview."
+pt_err_cant_open = u"Impossible to open %s"
+pt_err_cant_downld = u"Impossible to download %s"
+pt_err_cant_pst_cont = u"Impossible to download post contents"
+pt_err_unknown_ext = u"Unkown externsion for %s"
+pt_err_cant_tiny_url = u"Impossible to create tiny URL."
+pt_err_cant_send_twitter = u"Impossible to send post title to twitter."
+        
+# Posts popup menu
+pt_pmenu_posts = u"Posts:"
+pt_pmenu_del_post = u"Delete post ?"
+pt_pmenu_res = u"Resolution ?"
+pt_pmenu_flash = u"Flash ?"
+pt_pmenu_img_src = u"Image from:"
+pt_pmenu_img_url = u"Image URL:"
+pt_pmenu_link_url = u"Link URL:"
+pt_pmenu_linkyt_url = u"Youtube URL:"
+pt_pmenu_send_post = u"Publish post ?"
+pt_pmenu_post_title = u"Post title:"
+pt_pmenu_images = u"Images"
+pt_pmenu_scale_img = u"Scale this image?"
+pt_pmenu_scale_factor = u"Scale factor (%)"
+pt_pmenu_tags = u"Tags:"
+pt_pmenu_cats = u"Categories:"
+
+# Settings info
+st_info_blog_set = u"Blog settings"
+st_info_proxy_set = u"Proxy settings"
+st_info_twitter_set = u"Twitter settings"
+st_info_blog_acc_set = u"Blog account settings"
+        
+# Settings error
+st_err_no_access_point = u"Could't find any access point."
+st_err_one_ap_req = u"At least one access point is required."
+        
+# Settings query
+st_query_access_points = u"Access points:"
+st_query_proxy_add = u"Proxy address:"
+st_query_proxy_prt = u"Proxy port:"
+st_query_proxy_usr = u"Proxy username:"
+st_query_proxy_pwd = u"Proxy password:"
+st_query_twitter_usr = u"Twitter user:"
+st_query_twitter_pwd = u"Twitter password:"
+        
+# Settings menu
+st_menu_canc = u"Cancel"
+st_menu_blog = u"Blog Accounts"
+st_menu_proxy = u"Proxy"
+st_menu_access_point = u"Access Point"
+st_menu_lang = u"Language"
+st_menu_twitter = u"Twitter"
+st_menu_blog_acc = u"Account name:"
+st_menu_blog_url = u"Blog URL:"
+st_menu_blog_usr = u"Username:"
+st_menu_blog_pwd = u"Password:"
+st_menu_blog_npt = u"Number of posts:"
+st_menu_blog_cpp = u"Number of comments per post:"
+st_menu_blog_eml = u"Email (for comments):"
+st_menu_blog_rnm = u"Real name (for comments):"
+st_menu_proxy_ena = u"Enabled"
+st_menu_proxy_add = u"Address"
+st_menu_proxy_prt = u"Port"
+st_menu_proxy_usr = u"Username"
+st_menu_proxy_pwd = u"Password"
+st_menu_proxy_on = u"Enabled"
+st_menu_proxy_off = u"Disabled"
+st_menu_twitter_ena = u"Send to twitter"
+st_menu_twitter_on = u"Yes"
+st_menu_twitter_off = u"No"
+st_menu_new_blog = u"New"
+st_menu_cpy_blog = u"Copy"
+st_menu_del_blog = u"Delete"
+st_menu_edt_blog = u"Edit"
+            
+# languages support
+st_menu_en_us = u"English (USA)"
+st_menu_pt_br = u"Portuguese (Brazil)"
+st_menu_es = u"Spanish"
+st_menu_tr = u"Turkish"
+st_menu_it = u"Italian"
+st_menu_nl = u"Dutch"
+st_menu_de = u"German"
+st_menu_ro = u"Romanian"
+st_menu_zh_cn = u"Simplified Chinese"
+        
+# Setting popup menu
+st_pmenu_lang = u"Language:"
+
+# Window
+wi_info_exit = u"Exit"
+
+# Wpwrapper error
+wp_err_cant_del_cat = u"Impossible to delete category %s."
+wp_err_cant_create_cat = u"Impossible to create category %s."
+wp_err_cant_downl_cat =u"Impossible to retrieve the categories list."
+wp_err_cant_updt_post = u"Impossible to update posts."
+wp_err_cant_downl_post = u"Impossible to download the post. Try again."
+wp_err_cant_upld_img = u"Impossible to upload %s. Try again."
+wp_err_cant_pub_post = u"Impossible to publish the post. Try again."
+wp_err_cant_updt_post_list = u"Impossible to update post title list. Try again."
+wp_err_cant_updt_the_post = u"Impossible to update the post. Try again."
+wp_err_cant_downl_cmt = u"Impossible to download comments. Try again."
+wp_err_cant_updt_cmt = u"Impossible to update the comment. Try again."
+wp_err_cant_updt_cmt_list = u"Impossible to update the comment list. Try again."
+wp_err_cant_pub_cmt = u"Impossible to publish the comment. Try again."
+wp_err_cant_appr_cmt = u"Impossible to approve the comment. Try again."
+wp_err_cant_del_cmt = u"Impossible to delete the comment. Try again."
+
+# Wpwrapper info
+wp_info_cat_del = u"Category %s deleted."
+wp_info_upld_img = u"Uploading %s..."
+wp_info_upld_post_cont = u"Uploading post contents..."
+wp_info_updt_post_list = u"Updating post list..."
+wp_info_cmt_approved = u"Comment approved."
+wp_info_cmt_del = u"Comment deleted."
+        
+# Wpwrapper list
+wp_list_uncategorized = u"Uncategorized"
+
+# Tags main menu
+tg_menu_updt = u"Update"
+tg_menu_clos = u"Close"
+
+# Tags info messages
+tg_info_tag_pos = u"[%d/%d] Tags"
+tg_info_downld_tags = u"Downloading tags..."
+tg_info_udt_tags_lst = u"Please, update the tag list."
+
+# Tags error messages        
+tg_err_cant_updt_tags = u"Impossible to update tags."
+
+# Tags popup menu
+tg_pmenu_cats = u"Tags:"
