9fd5b069fb47db76b17a540ce18b667ae7cf45ac
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-06-17 03:25:05 +0000

    support for 1.9.5 only and trying to add support for card install

--- trunk/wordmobi/create_sis.bat
+++ trunk/wordmobi/create_sis.bat
@@ -14,29 +14,26 @@ SET ZIP="C:\Arquivos de programas\7-Zip\7z.exe"
 REM Path to module-repo, inside Python For S60 
 SET PYS60DIR=D:\S60\python1_9_5
 
-
 SET OPTS=--verbose --uid=0xefefefef --version="%1" ^
          --appname="%APPNAME%" --textfile="%TXTFILE%" --icon="%ICON%" ^
-         --caps="%CAPBLS%" "%TMPDIR%"
+         --extrasdir=extras --heapsize=4k,5M --caps="%CAPBLS%"
      
-echo "Generating for Python 1.4.5..."
-rmdir /s /q %TMPDIR%
-mkdir %TMPDIR%
-copy  %SRCDIR%\*.py  %TMPDIR%
-
-%PYTHON% ensymble.py py2sis %OPTS% --heapsize=4k,2M "%APPNAME%-%1.sis"
-
-echo "Generating for Python 1.9.4..."
-rmdir /s /q %TMPDIR%
-mkdir %TMPDIR%\extras\data\python
-copy  %SRCDIR%\*.py  %TMPDIR%\extras\data\python
-move %TMPDIR%\extras\data\python\default.py  %TMPDIR%\
+echo "Generating for Python 1.9.5"
+echo "Copying default modules"
 if not exist .\module-repo\ xcopy /E "%PYS60DIR%\module-repo" .\module-repo\
 if not exist .\templates\   xcopy /E "%PYS60DIR%\templates"   .\templates\
 
-%PYTHON% ensymble_py19.py py2sis %OPTS% --extrasdir=extras --heapsize=4k,5M "%APPNAME%-%1-py19.sis"
+echo "populating temp dir"
+rmdir /s /q %TMPDIR%
+mkdir %TMPDIR%\extras\data\python\wordmobidir\loc
+copy  %SRCDIR%\*.py  %TMPDIR%\extras\data\python\wordmobidir
+copy  %SRCDIR%\loc\*.py  %TMPDIR%\extras\data\python\wordmobidir\loc
+copy  %SRCDIR%\wordmobi.mif  %TMPDIR%\extras\data\python\wordmobidir
+move %TMPDIR%\extras\data\python\wordmobidir\default.py  %TMPDIR%\
 
-%ZIP% a -tzip %APPNAME%-%1-src.zip src\*.py
+%PYTHON% ensymble.py py2sis %OPTS% "%TMPDIR%" "%APPNAME%-%1-py195.sis"
+
+%ZIP% a -r -tzip %APPNAME%-%1-src.zip src\*.py src\*.mif
 
 goto end
 
