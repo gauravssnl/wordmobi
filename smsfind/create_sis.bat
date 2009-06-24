a8526a8b24c24d1252fa51e0fc14ff4b8d57b575
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-06-24 22:15:24 +0000

    First import

---
+++ smsfind/create_sis.bat
@@ -0,0 +1,18 @@
+@echo off
+
+SET VERSION=1.0.0
+SET PYTHON=C:\Python25\python
+SET APPNAME=SMSFind
+SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
+SET PYS60DIR=D:\S60\python1_9_5
+
+if not exist .\module-repo\ xcopy /E "%PYS60DIR%\module-repo" .\module-repo\
+if not exist .\templates\   xcopy /E "%PYS60DIR%\templates"   .\templates\
+
+mkdir .\smsfind_sis\root\data\python\smsfind
+copy  .\src\*.py   .\smsfind_sis\root\data\python\smsfind
+move  .\smsfind_sis\root\data\python\smsfind\default.py   .\smsfind_sis
+
+%PYTHON% .\ensymble.py py2sis --version="%VERSION%" --heapsize=4k,5M ^
+          --appname="%APPNAME%" --caps="%CAPBLS%" ^
+          --extrasdir=root smsfind_sis "%APPNAME%%VERSION%.sis"
---
+++ smsfind/src/smsfind.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Marcelo Barros de Almeida
+# marcelobarrosalmeida@gmail.com
+# License: GPL 3
+
+from window import *
+from appuifw import *
+import inbox
+import time
+
+class ShowSMS(Dialog):
+    def __init__(self,cbk,title,msg):
+        Dialog.__init__(self,cbk,title,Text(msg))
+        
+class SMSFind(Application):
+    def __init__(self):
+        self.dlg = None
+        self.results = [u""]
+        self.terms = u""
+        body = Canvas()
+        menu = [(u"Find", self.get_pattern),
+                (u"About", self.about),
+                (u"Exit", self.close_app)]
+        Application.__init__(self,u"SMS Find",body,menu)
+
+    def about(self):
+        note( u"SMS Find.\nMarcelo Barros de Almeida\nmarcelobarrosalmeida@gmail.com", "info" )
+
+    def lst_cbk(self):
+        idx = self.body.current()
+        (sms_id,txt,tmr,addr,fn) = self.results[idx]
+        msg = u"Address: " + addr + \
+              u"\nDate: " + tmr + \
+              u"\nBox: " + fn + \
+              u"\n\n" + txt
+        self.dlg = ShowSMS(lambda:self.refresh(),unicode(sms_id),msg)
+        self.dlg.run()
+        
+    def bmh_search(self, pattern, text):
+        # http://code.activestate.com/recipes/117223/
+        m = len(pattern)
+        n = len(text)
+        if m > n: return -1
+        skip = []
+        for k in range(256): skip.append(m)
+        for k in range(m - 1): skip[ord(pattern[k])] = m - k - 1
+        skip = tuple(skip)
+        k = m - 1
+        while k < n:
+            j = m - 1; i = k
+            while j >= 0 and text[i] == pattern[j]:
+                j -= 1; i -= 1
+            if j == -1: return i + 1
+            k += skip[ord(text[k])]
+        return -1
+        
+    def find(self,pattern):
+        self.results = []
+        lst = []
+        folders = {inbox.EInbox:u"Inbox",
+                   inbox.EOutbox:u"Outbox",
+                   inbox.ESent:u"Sent",
+                   inbox.EDraft:u"Draft"}
+        for f,fn in folders.iteritems():
+            ibx = inbox.Inbox(f)
+            for sms_id in ibx.sms_messages():
+                txt = ibx.content(sms_id)
+                txt_utf8 = txt.encode('utf-8')
+                pattern_utf8 = pattern.encode('utf-8')
+                if self.bmh_search(pattern_utf8.lower(),txt_utf8.lower()) > -1:
+                    dt = unicode(time.ctime(ibx.time(sms_id)),'utf-8',errors='ignore')
+                    self.results.append((sms_id,txt,dt,ibx.address(sms_id),fn))
+                    lst.append((dt,txt[:50]))
+        if self.results:
+            self.body = Listbox(lst,self.lst_cbk)
+            self.refresh()
+        else:
+            note(u"No results for " + self.terms,"info")
+            
+    def get_pattern(self):
+        pattern = query(u"Search terms:", "text", self.terms)
+        if pattern is not None:
+            if pattern:
+                pattern = pattern.strip()
+                self.terms = pattern
+                self.find(pattern)  
+
+if __name__ == "__main__":
+    sms = SMSFind()
+    sms.run()
