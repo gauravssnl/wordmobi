98af0ad132abbc268065b9643c17dfa7501e759d
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-06-24 03:14:46 +0000

    improving interface with text dialog and searching in all inboxes

--- smsfind/smsfind.py
+++ smsfind/smsfind.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
+from window import *
 from appuifw import *
-import e32
 import inbox
 import time
 
-class SMSFind:
-
+class ShowSMS(Dialog):
+    def __init__(self,cbk,title,msg):
+        Dialog.__init__(self,cbk,title,Text(msg))
+        
+class SMSFind(Application):
     def __init__(self):
-        self.inbox_results = []
-        app.exit_key_handler = self.close
-        app.title = u"SMS Find"
-        app.menu = [(u"Find", self.get_pattern),
-                    (u"About", self.about),
-                    (u"Exit", self.close)]
-        app.body = Canvas()
-        self.app_lock = e32.Ao_lock()
-        self.app_lock.wait()
-
-    def close(self):
-        self.app_lock.signal()
+        self.dlg = None
+        self.results = [u""]
+        self.terms = u""
+        body = Canvas()
+        menu = [(u"Find", self.get_pattern),
+                (u"About", self.about),
+                (u"Exit", self.close_app)]
+        Application.__init__(self,u"SMS Find",body,menu)
 
     def about(self):
-        note( u"SMS Find.\nMarcelo Barros\nmarcelobarrosalmeida@gmail.com", "info" )
+        note( u"SMS Find.\nMarcelo Barros de Almeida\nmarcelobarrosalmeida@gmail.com", "info" )
 
     def lst_cbk(self):
-        idx = app.body.current()
-        (sms_id, txt, tmr, addr) = self.inbox_results[idx]
+        idx = self.body.current()
+        (sms_id,txt,tmr,addr) = self.results[idx]
         msg = u"From: " + addr + u"\n"
-        msg = msg + u"Date: " + tmr + u"\n"
+        msg = msg + u"Date: " + tmr + u"\n\n"
         msg = msg + txt
-        note( msg, "info" )
+        self.dlg = ShowSMS(lambda:self.refresh(),unicode(sms_id),msg)
+        self.dlg.run()
         
     def bmh_search(self, pattern, text):
         # http://code.activestate.com/recipes/117223/
@@ -50,24 +50,34 @@ class SMSFind:
         return -1
         
     def find(self,pattern):
-        self.inbox_results = []
+        self.results = []
         lst = []
-        ibx = inbox.Inbox(inbox.EInbox)
-        for sms_id in ibx.sms_messages():
-            txt = ibx.content(sms_id)
-            if self.bmh_search(pattern.lower(),txt.lower()) > -1:
-                tmr = unicode(time.ctime(ibx.time(sms_id)),'utf-8',errors='ignore')
-                self.inbox_results.append((sms_id, txt, tmr,ibx.address(sms_id)))
-                lst.append(txt[:50])
-        if self.inbox_results:
-            app.body = Listbox(lst,self.lst_cbk)
+        folders = [inbox.EInbox,inbox.EOutbox,inbox.ESent,inbox.EDraft]
+        for f in folders:
+            ibx = inbox.Inbox(f)
+            for sms_id in ibx.sms_messages():
+                txt = ibx.content(sms_id)
+                print "->",txt
+                txt_utf8 = txt.encode('utf-8')
+                pattern_utf8 = pattern.encode('utf-8')
+                if self.bmh_search(pattern_utf8.lower(),txt_utf8.lower()) > -1:
+                    tmr = unicode(time.ctime(ibx.time(sms_id)),'utf-8',errors='ignore')
+                    self.results.append((sms_id,txt,tmr,ibx.address(sms_id)))
+                    lst.append(txt[:50])
+        if self.results:
+            self.body = Listbox(lst,self.lst_cbk)
+            self.refresh()
+        else:
+            note(u"No results for " + self.terms,"info")
             
     def get_pattern(self):
-        pattern = query(u"Find what?", "text")
+        pattern = query(u"Search terms:", "text", self.terms)
         if pattern is not None:
             if pattern:
                 pattern = pattern.strip()
+                self.terms = pattern
                 self.find(pattern)  
 
 if __name__ == "__main__":
     sms = SMSFind()
+    sms.run()
