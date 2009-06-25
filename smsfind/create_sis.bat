f2cd89ed3b5e844add08f316f6980d0e9ed88605
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-06-25 00:01:19 +0000

    renaming to smssearch

--- smsfind/create_sis.bat
+++ smsfind/create_sis.bat
@@ -2,17 +2,17 @@
 
 SET VERSION=1.0.0
 SET PYTHON=C:\Python25\python
-SET APPNAME=SMSFind
+SET APPNAME=SMSearch
 SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
 SET PYS60DIR=D:\S60\python1_9_5
 
 if not exist .\module-repo\ xcopy /E "%PYS60DIR%\module-repo" .\module-repo\
 if not exist .\templates\   xcopy /E "%PYS60DIR%\templates"   .\templates\
 
-mkdir .\smsfind_sis\root\data\python\smsfind
-copy  .\src\*.py   .\smsfind_sis\root\data\python\smsfind
-move  .\smsfind_sis\root\data\python\smsfind\default.py   .\smsfind_sis
+mkdir .\smsearch_sis\root\data\python\smsearch
+copy  .\src\*.py   .\smsearch_sis\root\data\python\smsearch
+move  .\smsearch_sis\root\data\python\smsearch\default.py   .\smsearch_sis
 
 %PYTHON% .\ensymble.py py2sis --version="%VERSION%" --heapsize=4k,5M ^
           --appname="%APPNAME%" --caps="%CAPBLS%" ^
-          --extrasdir=root smsfind_sis "%APPNAME%%VERSION%.sis"
+          --extrasdir=root smsearch_sis "%APPNAME%%VERSION%.sis"
--- smsfind/src/default.py
+++ smsfind/src/default.py
@@ -5,12 +5,12 @@ import os
 # looking for install dir   
 DEFDIR = u""
 for d in e32.drive_list():
-    appd = d + u"\\data\\python\\smsfind\\"
-    if os.path.exists(appd + u"smsfind.py"):
+    appd = d + u"\\data\\python\\smsearch\\"
+    if os.path.exists(appd + u"smsearch.py"):
         DEFDIR = appd
         break
 
 if DEFDIR:
     sys.path.append(appd)
-    from smsfind import SMSFind
-    SMSFind().run()
+    from smsearch import SMSearch
+    SMSearch().run()
