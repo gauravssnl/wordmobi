50bdaf10e07c0dd8857822ea2a2c7755e32b47a7
marcelobarrosalmeida@gmail.com <marcelobarrosalmeida@gmail.com@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-07-19 18:37:54 +0000

    New builder, using pys60 install dir instead copying stuff from there. TMPDIR variable can not be used otherwise ensymble will fail

--- trunk/wordmobi/create_sis.bat
+++ trunk/wordmobi/create_sis.bat
@@ -6,34 +6,49 @@ SET PYTHON=C:\Python25\python.exe
 SET APPNAME=Wordmobi
 SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
 SET SRCDIR=src
-SET TXTFILE=README
-SET TMPDIR=src.tmp
+SET WMTMPDIR=src.tmp
 SET ICON=wordmobi.svg
 REM put you zip tool here
 SET ZIP="C:\Arquivos de programas\7-Zip\7z.exe"
 REM Path to module-repo, inside Python For S60 
-SET PYS60DIR=D:\S60\python1_9_5
+SET PYS60DIR=C:\Arquivos de programas\PythonForS60
 
-SET OPTS=--verbose --uid=0xefefefef --version="%1" ^
-         --appname="%APPNAME%" --textfile="%TXTFILE%" --icon="%ICON%" ^
-         --extrasdir=extras --heapsize=4k,5M --caps="%CAPBLS%"
-     
-echo "Generating for Python 1.9.5"
-echo "Copying default modules"
-if not exist .\module-repo\ xcopy /E "%PYS60DIR%\module-repo" .\module-repo\
-if not exist .\templates\   xcopy /E "%PYS60DIR%\templates"   .\templates\
+SET OPTS=--verbose  --version="%1" --uid=0xefefefef ^
+--appname="%APPNAME%" --icon="%ICON%" ^
+--extrasdir=extras --heapsize=4k,5M --caps=%CAPBLS%
 
-echo "populating temp dir"
-rmdir /s /q %TMPDIR%
-mkdir %TMPDIR%\extras\data\python\wordmobidir\loc
-copy  %SRCDIR%\*.py  %TMPDIR%\extras\data\python\wordmobidir
-copy  %SRCDIR%\loc\*.py  %TMPDIR%\extras\data\python\wordmobidir\loc
-copy  %SRCDIR%\wordmobi.mif  %TMPDIR%\extras\data\python\wordmobidir
-move %TMPDIR%\extras\data\python\wordmobidir\default.py  %TMPDIR%\
+echo "Populating temp dir"
+if exist "%WMTMPDIR%" rmdir /s /q "%WMTMPDIR%"
+mkdir %WMTMPDIR%\extras\data\python\wordmobidir\loc
+mkdir %WMTMPDIR%\extras\data\python\wordmobidir\res
+copy  %SRCDIR%\*.py  %WMTMPDIR%\extras\data\python\wordmobidir
+copy  %SRCDIR%\loc\*.py  %WMTMPDIR%\extras\data\python\wordmobidir\loc
+copy  %SRCDIR%\res\*.png  %WMTMPDIR%\extras\data\python\wordmobidir\res
+copy  %SRCDIR%\wordmobi.mif  %WMTMPDIR%\extras\data\python\wordmobidir
+move %WMTMPDIR%\extras\data\python\wordmobidir\default.py  %WMTMPDIR%\
+  
+echo "Copying project to PyS60 dir"
+if exist "%PYS60DIR%\%WMTMPDIR%" rmdir /s/q "%PYS60DIR%\%WMTMPDIR%"
+xcopy /E "%WMTMPDIR%" "%PYS60DIR%\%WMTMPDIR%\"
+if exist "%PYS60DIR%\%ICON%" del /s/q "%PYS60DIR%\%ICON%"
+copy "%ICON%" "%PYS60DIR%\%ICON%"
 
-%PYTHON% ensymble.py py2sis %OPTS% "%TMPDIR%" "%APPNAME%-%1-py195.sis"
+echo "Generating for Python 1.9.x"
+pushd .
+cd "%PYS60DIR%"
+echo "Creating sis"
+%PYTHON% ensymble.py py2sis %OPTS% "%WMTMPDIR%" "%APPNAME%-%1.sis"
 
-%ZIP% a -r -tzip %APPNAME%-%1-src.zip src\*.py src\*.mif
+popd
+copy "%PYS60DIR%\%APPNAME%-%1.sis" .
+echo "Zipping source files"
+%ZIP% a -r -tzip %APPNAME%-%1-src.zip src\*.py src\*.mif src\*.png
+
+echo "Erasing"
+rmdir /s/q "%PYS60DIR%\%WMTMPDIR%"
+del /s/q "%PYS60DIR%\%ICON%"
+del "%PYS60DIR%\%APPNAME%-%1.sis"
+rmdir /s/q "%WMTMPDIR%"
 
 goto end
 
@@ -41,4 +56,5 @@ goto end
 echo Sintaxe: %0 version
 
 :end
+
 
