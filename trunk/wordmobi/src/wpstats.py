83cf6ecebd095621ddf2bc0e982b17d304891286
marcelobarrosalmeida@gmail.com <marcelobarrosalmeida@gmail.com@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-08-05 18:11:02 +0000

    begin and end redraw function (Pys60 1.9.7) added

--- trunk/wordmobi/src/wpstats.py
+++ trunk/wordmobi/src/wpstats.py
@@ -263,6 +263,7 @@ class Stats(Dialog):
         self.body.blit(self.scr_buf)
         
     def stats_buffer_redraw(self,rect=None):
+        self.body.begin_redraw()
         self.clear_screen()
         self.draw_grid()
         self.draw_points()
@@ -270,6 +271,7 @@ class Stats(Dialog):
         self.draw_selection()
         self.draw_toolbar()
         self.stats_canvas_redraw()
+        self.body.end_redraw()
    
     def stats_resize(self,rect=None):
         if self.scr_buf:
