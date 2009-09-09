6031a8f755eb3c0b311f381533c8dabcd0e44621
marcelobarrosalmeida@gmail.com <marcelobarrosalmeida@gmail.com@011f3a0a-923e-11dd-8361-953bea5d279d>
2009-09-09 17:24:06 +0000

    Adding strategy for pre-compiling py files (commented out)

--- trunk/wordmobi/src/default.py
+++ trunk/wordmobi/src/default.py
@@ -5,14 +5,29 @@ import os
 # looking for install dir   
 DEFDIR = u""
 for d in e32.drive_list():
-    appd = d + u"\\data\\python\\wordmobidir\\"
-    if os.path.exists(appd + u"wordmobi.py"):
+    appd = os.path.join(d,u"\\data\\python\\wordmobidir\\")
+    if os.path.exists(os.path.join(appd,u"wordmobi.py")):
         DEFDIR = appd
         break
 
 if DEFDIR:
-    # running wordmobi
+    # fixing paths
     sys.path.append(DEFDIR)
-    sys.path.append(os.path.join(DEFDIR,u"loc"))    
+    sys.path.append(os.path.join(DEFDIR,u"loc"))
+    
+    # pre compile all .py files to speedup next startups
+    #if not os.path.exists(os.path.join(DEFDIR,u"wordmobi.pyc")):
+    #    import py_compile
+    #    [ py_compile.compile(os.path.join(DEFDIR,pf))
+    #      for pf in os.listdir(DEFDIR)
+    #      if pf.endswith('.py') ]
+    #    [ py_compile.compile(os.path.join(DEFDIR,u"loc",pf))
+    #      for pf in os.listdir(os.path.join(DEFDIR,u"loc"))
+    #      if pf.endswith('.py') ]
+    
+    # starting wordmobi
     import wordmobi
     wordmobi.WordMobi().run()
+else:
+    import appuifw
+    appuifw.note(u"Could not start Wordmobi","error")    
