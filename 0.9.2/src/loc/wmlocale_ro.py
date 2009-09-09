03f8d09cac50249cdf10ad1dad4d7c65b6e348c3
marcelobarrosalmeida <marcelobarrosalmeida@gmail.com>
2009-09-09 17:50:34 +0000

    version 0.9.2

---
+++ 0.9.2/src/loc/wmlocale_ro.py
@@ -0,0 +1,299 @@
+# -*- coding: cp1252 -*-
+
+# Translation by:
+#
+# Adinel Giboi <adinel.giboi (at) gmail.com>
+
+# Promo phrase
+promo_phrase = u"<br><br>Articol scris cu <a href=\"http://wordmobi.googlecode.com\">Wordmobi</a>" 
+
+# Wordmobi main menu
+wm_menu_post = u"Articole"
+wm_menu_comm = u"Comentarii"
+wm_menu_tags = u"Taguri"
+wm_menu_cats = u"Categorii"
+wm_menu_sets = u"Setari"
+wm_menu_upgr = u"Actualizare"
+wm_menu_abou = u"Despre"
+wm_menu_exit = u"Iesire"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Neimplementat inca."
+wm_err_upd_page = u"Nu pot accesa pagina de actualizare %s"
+wm_err_downld_fail = u"Nu am putut descarca %s"
+wm_err_upd_info = u"Lipsa informatii de actualizare."
+wm_err_cache_cleanup = u"Nu au putut fi sterse toate fisierele din %s. Incercati mai tarziu."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Verific pagina de actualizare..."
+wm_info_ver_is_updt = u"Aceasta versiune este cea mai recenta."
+wm_info_downloading = u"Descarcare ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s descarcat in %supdates. Va rugam sa instalati."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Descarcare %s ?"
+wm_pmenu_exit = u"Parasiti aplicatia?"
+
+# General messages
+gm_yes = u"Da"
+gm_no = u"Nu"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"Editor Wordpress"
+ab_menu_wordmobi_auth = u"Autor"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Cod sursa"
+ab_menu_wordmobi_blog = u"Blog"
+ab_menu_wordmobi_lics = u"Licenta"
+ab_menu_wordmobi_warn = u"Avertizare"
+ab_menu_wordmobi_warn_val = u"Utilizati pe riscul dumneavoastra"
+
+# Categories main menu
+ca_menu_updt = u"Actualizare"
+ca_menu_dele = u"Sterge"
+ca_menu_cnew = u"Categorie noua"
+ca_menu_clos = u"Inchide"
+
+# Categories popup menu
+ca_pmenu_cats = u"Categorii:"
+ca_pmenu_delete = u"Sterge %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Categorii"
+ca_info_downld_cats = u"Actualizare categorii..."
+ca_info_del_cat = u"Stergere categorie %s ..."
+ca_info_create_cat = u"Creare categorie %s ..."
+
+# categories queries
+ca_query_cat_name = u"Numele categoriei:"
+
+# Comments main menu
+cm_menu_updt = u"Actualizare"
+cm_menu_view = u"Vizualizare/Editare"
+cm_menu_dele = u"Sterge"
+cm_menu_cnew = u"Comentariu nou/Raspunde"
+cm_menu_clos = u"Inchide"
+cm_menu_canc = u"Anulare"
+cm_menu_pstt = u"Titlul articolului"
+cm_menu_cont = u"Continut"
+cm_menu_name = u"Nume"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Website"
+
+# Comments lists
+cm_list_any = u"Toate"
+cm_list_spam = u"Spam"
+cm_list_moderated = u"Moderate"
+cm_list_unmoderated = u"Nemoderate"
+cm_list_approve = u"Aproba"
+cm_list_one_post = u"Un articol"
+cm_list_all_posts = u"Toate articolele"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Comentarii"
+cm_info_aprv_cmt = u"Aprobare comentariu %s"
+cm_info_downld_pt = u"Descarcare titlurile articole..."
+cm_info_which_post = u"Pentru care articol?"
+cm_info_downld_cmts = u"[%d/%d] Descarcare comentarii ..."
+cm_info_downld_cmt = u"Descarcare comentarii ..."
+cm_info_no_cmts_st = u"Niciun comentariu cu statusul '%s'."
+cm_info_udt_cmts_lst = u"Va rugam actualizati lista de comentarii."
+cm_info_del_cmt = u"Sterge comentariu %s"
+cm_info_upld_cmt = u"Incarcare comentariu ..."
+cm_info_updt_cmt = u"Actualizare comentariu ..."
+cm_info_undef = u"Nedefinit"
+cm_info_empty = u"<gol>"
+cm_info_cmt_contents = u"Continutul comentariului"
+cm_info_new_cmt = u"Comentariu Nou"
+cm_info_edit_cmt = u"Editeaza Comentariu"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Statusul comentariului:"
+cm_pmenu_updt_for = u"Actualiz. coment. pentru?"
+cm_pmenu_downld_fail = u"Desc. esuata ! Continuati?"
+cm_pmenu_del_cmt = u"Stergere comentariu ?"
+cm_pmenu_send_cmt = u"Trimite comentariu ?"
+cm_pmenu_updt_cmt = u"Actualizare comentariu ?"
+cm_pmenu_comts = u"Comentarii:"
+
+# Posts main menu
+pt_menu_updt = u"Actualizare"
+pt_menu_view = u"Vizualizare/Editare"
+pt_menu_dele = u"Sterge"
+pt_menu_lstc = u"Afiseaza Comentariile"
+pt_menu_cnew = u"Articol nou"
+pt_menu_offl_publ = u"Publica"
+pt_menu_clos = u"Inchide"
+pt_menu_pict = u"Fa o fotografie"
+pt_menu_canc = u"Anulare"
+pt_menu_text = u"Text"
+pt_menu_refs = u"Linkuri/imagini"
+pt_menu_lsts = u"Liste"
+pt_menu_revs = u"Versiune"
+pt_menu_prvw = u"Previzualizare"
+pt_menu_titl = u"Titlu"
+pt_menu_cont = u"Continut"
+pt_menu_cats = u"Categorii"
+pt_menu_imgs = u"Imagini"
+pt_menu_pubs = u"Publica"
+pt_menu_s2tw = u"Trimite catre Twitter"
+
+# Posts lists
+pt_list_320x240 = u"(320x240)"
+pt_list_640x480 = u"(640x480)"
+pt_list_fsh_auto = u"Auto"
+pt_list_fsh_none = u"Nimic"
+pt_list_fsh_forc = u"Fortat"
+pt_list_loc_file = u"Fisier local"
+pt_list_url = u"URL"
+pt_list_ins_img = u"Inserare"
+pt_list_take_img = u"Fa o fotografie"
+pt_list_view_img = u"Vizualizare/Listare"
+pt_list_rem_img = u"Sterge"
+pt_list_save_it = u"Nu, dar salveaza"
+pt_list_yes_rem_pst = u"Da, sterge articolul"
+pt_list_yes_loc_ch = u"Da, doar modificarile locale"
+pt_list_yes_del_all = u"Da, sterge tot"
+
+# Posts info
+pt_info_pst_pos = u"[%d/%d] Articole"
+pt_info_downld_pt = u"Descarcare titluri articole..."
+pt_info_no_posts = u"Niciun articol disponibil."
+pt_info_del_post = u"Sterge articol..."
+pt_info_post_del = u"Articol sters."
+pt_info_updt_pst_lst = u"Va rugam actualizati lista de articole."
+pt_info_downld_post = u"Descarcare articol..."
+pt_info_empty = u"<gol>"
+pt_info_pict = u"Fa o fotografie"
+pt_info_post_contents = u"Continut Articol"
+pt_info_new_post = u"Articol nou"
+pt_info_edit_post = u"Editare Articol"
+pt_info_no_pub = u"Da"
+pt_info_draft = u"Nu (sablon)"
+pt_info_no_imgs_sel = u"Nicio imagine selectata."
+pt_info_downld_img = u"Descarcare %s"
+pt_info_send_twt = u"Trimitere titlu articol catre Twitter"
+pt_info_twitter_updated = u"Twitter actualizat."
+
+# Posts error messages
+pt_err_cant_del_pt = u"Imposibil de sters articol."
+pt_err_cant_start_viewf = u"Nu am putut porni vizorul."
+pt_err_cant_stop_viewf = u"Nu am putut opri vizorul."
+pt_err_cant_take_pic = u"Nu am putut face nicio fotografie."
+pt_err_cant_gen_prvw = u"Nu am putut genera pre-vizualizarea."
+pt_err_cant_prvw = u"Pre-vizualizare imposibila."
+pt_err_cant_open = u"Nu pot deschide %s"
+pt_err_cant_downld = u"Nu pot descarca %s"
+pt_err_cant_pst_cont = u"Nu pot descarca continutul articolului"
+pt_err_unknown_ext = u"Extensie necunoscuta pentru %s"
+pt_err_cant_tiny_url = u"Nu am putut crea tinyURL."
+pt_err_cant_send_twitter = u"Nu am putut trimite titlul articolului catre Twitter."
+
+# Posts popup menu
+pt_pmenu_posts = u"Articole:"
+pt_pmenu_del_post = u"Sterge articol?"
+pt_pmenu_res = u"Rezolutie?"
+pt_pmenu_flash = u"Blit?"
+pt_pmenu_img_src = u"Imagine de la:"
+pt_pmenu_img_url = u"URL imagine:"
+pt_pmenu_link_url = u"URL link:"
+pt_pmenu_linkyt_url = u"URL Youtube:"
+pt_pmenu_send_post = u"Publica articol ?"
+pt_pmenu_post_title = u"Titlul articolului:"
+pt_pmenu_images = u"Imagini"
+pt_pmenu_scale_img = u"Modificare marime imagine?"
+pt_pmenu_scale_factor = u"Factor de marime (%)"
+
+# Settings info
+st_info_blog_set = u"Setari blog"
+st_info_proxy_set = u"Setari Proxy"
+st_info_twitter_set = u"Setari Twitter"
+st_info_blog_acc_set = u"Setari cont blog"
+
+# Settings error
+st_err_no_access_point = u"Nu am gasit niciun punct de acces."
+st_err_one_ap_req = u"Este necesar cel putin un punct de acces."
+
+# Settings query
+st_query_access_points = u"Puncte de acces:"
+st_query_proxy_add = u"Adresa Proxy:"
+st_query_proxy_prt = u"Port Proxy:"
+st_query_proxy_usr = u"Nume utilizator Proxy:"
+st_query_proxy_pwd = u"Parola Proxy:"
+st_query_twitter_usr = u"Nume utilizator Twitter:"
+st_query_twitter_pwd = u"Parola Twitter:"
+
+# Settings menu
+st_menu_canc = u"Anulare"
+st_menu_blog = u"Bloguri"
+st_menu_proxy = u"Proxy"
+st_menu_access_point = u"Punct de Acces"
+st_menu_lang = u"Limba"
+st_menu_twitter = u"Twitter"
+st_menu_blog_acc = u"Nume cont:"
+st_menu_blog_url = u"URL Blog:"
+st_menu_blog_usr = u"Nume utilizator:"
+st_menu_blog_pwd = u"Parola:"
+st_menu_blog_npt = u"Numarul de articole:"
+st_menu_blog_cpp = u"Numarul de comentarii pe articole:"
+st_menu_blog_eml = u"Email (pentru comentarii):"
+st_menu_blog_rnm = u"Nume real (pentru comentarii):"
+st_menu_proxy_ena = u"Activat"
+st_menu_proxy_add = u"Adresa"
+st_menu_proxy_prt = u"Port"
+st_menu_proxy_usr = u"Nume utilizator"
+st_menu_proxy_pwd = u"Parola"
+st_menu_proxy_on = u"Activat"
+st_menu_proxy_off = u"Dezactivat"
+st_menu_twitter_ena = u"Trimite catre Twitter"
+st_menu_twitter_on = u"Da"
+st_menu_twitter_off = u"Nu"
+st_menu_new_blog = u"Nou"
+st_menu_cpy_blog = u"Copiere"
+st_menu_del_blog = u"Stergere"
+st_menu_edt_blog = u"Editare"
+
+# languages support
+st_menu_en_us = u"Engleza (SUA)"
+st_menu_pt_br = u"Portugheza (Brazilia)"
+st_menu_es = u"Spaniola"
+st_menu_tr = u"Turca"
+st_menu_it = u"Italiana"
+st_menu_nl = u"Olandeza"
+st_menu_de = u"Germana"
+st_menu_ro = u"Romana"
+
+# Setting popup menu
+st_pmenu_lang = u"Limba:"
+
+# Window
+wi_info_exit = u"Iesire"
+
+# Wpwrapper error
+wp_err_cant_del_cat = u"Nu am putut sterge categoria %s."
+wp_err_cant_create_cat = u"Nu am putut creea categoria %s."
+wp_err_cant_downl_cat =u"Nu am putut actualiza lista de categorii."
+wp_err_cant_updt_post = u"Nu am putut actualiza articolul."
+wp_err_cant_downl_post = u"Nu am putut descarca articolul. Mai incercati o data."
+wp_err_cant_upld_img = u"Nu am putut incarca %s. Mai incercati o data."
+wp_err_cant_pub_post = u"Nu am putut publica articolul. Mai incercati o data."
+wp_err_cant_updt_post_list = u"Nu am putut actualiza lista de titluri de articole. Mai incercati o data."
+wp_err_cant_updt_the_post = u"Nu am putut actualiza articolul. Mai incercati o data."
+wp_err_cant_downl_cmt = u"Nu am putut descarca comentariile. Mai incercati o data."
+wp_err_cant_updt_cmt = u"Nu am putut actualiza comentariu. Mai incercati o data."
+wp_err_cant_updt_cmt_list = u"Nu am putut actualiza lista de comentarii. Mai incercati o data."
+wp_err_cant_pub_cmt = u"Nu am putut publica comentariul. Mai incercati o data."
+wp_err_cant_appr_cmt = u"Nu am putut aproba comentariul. Mai incercati o data."
+wp_err_cant_del_cmt = u"Nu am putut sterge comentariul. Mai incercati o data."
+
+# Wpwrapper info
+wp_info_cat_del = u"Categoria %s a fost stearsa."
+wp_info_upld_img = u"Incarcare %s..."
+wp_info_upld_post_cont = u"Incarcare continut articol..."
+wp_info_updt_post_list = u"Actualizare lista articole..."
+wp_info_cmt_approved = u"Comentariu aprobat."
+wp_info_cmt_del = u"Comentariu sters."
+
+# Wpwrapper list
+wp_list_uncategorized = u"Fara categorie"
---
+++ 0.9.2/src/loc/wmlocale_ru.py
@@ -0,0 +1,355 @@
+# -*- coding: utf-8 -*-
+
+# Translation by:
+#
+# Cyrill Udartcev <cyrill (at) cyrill.co.uk>
+
+# Promo phrase
+promo_phrase = u"<br><br>Опубликовано с помощью <a href=\"http://wordmobi.googlecode.com\">Wordmobi</a>" 
+
+# Wordmobi main menu
+wm_menu_post = u"Записи"
+wm_menu_comm = u"Комментарии"
+wm_menu_cats = u"Рубрики"
+wm_menu_tags = u"Теги"
+wm_menu_sets = u"Настройки"
+wm_menu_upgr = u"Обновить"
+wm_menu_abou = u"О программе"
+wm_menu_exit = u"Выход"
+wm_menu_stat = u"Статистика"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Пока не поддерживается."
+wm_err_upd_page = u"Нет доступа к странице обновления %s"
+wm_err_downld_fail = u"Не удалось загрузить %s"
+wm_err_upd_info = u"Информация об обновлении отсутствует."
+wm_err_cache_cleanup = u"Не все файлы в %s удалось удалить. Попробуйте удалить их позже."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Проверка страницы обновлений..."
+wm_info_ver_is_updt = u"Ваша версия самая новая."
+wm_info_downloading = u"Загрузка ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s загружено в %supdates. Установите, пожалуйста."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Загрузить %s ?"
+wm_pmenu_exit = u"Выйти ?"
+
+# General messages
+gm_yes = u"Да"
+gm_no = u"Нет"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"Клиент Wordpress"
+ab_menu_wordmobi_auth = u"Автор"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Исходный код"
+ab_menu_wordmobi_blog = u"Блог"
+ab_menu_wordmobi_lics = u"Лицензия"
+ab_menu_wordmobi_warn = u"Внимание"
+ab_menu_wordmobi_warn_val = u"Используйте на свой страх и риск"
+
+# Categories main menu
+ca_menu_updt = u"Обновить"
+ca_menu_dele = u"Удалить"
+ca_menu_cnew = u"Создать новый"
+ca_menu_clos = u"Закрыть"
+
+# Categories popup menu
+ca_pmenu_cats = u"Рубрики:"
+ca_pmenu_delete = u"Удалить %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Рубрики"
+ca_info_downld_cats = u"Загрузка рубрик..."
+ca_info_del_cat = u"Удаление рубрики %s ..."
+ca_info_create_cat = u"Создание рубрики %s ..."
+
+# categories queries
+ca_query_cat_name = u"Имя рубрики:"
+
+# Comments main menu
+cm_menu_updt = u"Обновить"
+cm_menu_view = u"Просмотр/изменение"
+cm_menu_dele = u"Удалить"
+cm_menu_cnew = u"Создать новый/Ответить"
+cm_menu_clos = u"Закрыть"
+cm_menu_canc = u"Отмена"
+cm_menu_pstt = u"Название записи"
+cm_menu_cont = u"Содержание"
+cm_menu_name = u"Имя"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Сайт"
+
+# Comments lists
+cm_list_any = u"Любой"
+cm_list_spam = u"Спам"
+cm_list_moderated = u"Отмодерирован"
+cm_list_unmoderated = u"Не отмодерирован"
+cm_list_approve = u"Одобрить"
+cm_list_one_post = u"Одна запись"
+cm_list_all_posts = u"Все записи"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Комментарии"
+cm_info_aprv_cmt = u"Одобрение комментария %s"
+cm_info_downld_pt = u"Загрузка названий записей..."
+cm_info_which_post = u"Для какой записи?"
+cm_info_downld_cmts = u"[%d/%d] Загрузка комментариев ..."
+cm_info_downld_cmt = u"Загрузка комментариев ..."
+cm_info_no_cmts_st = u"Нет комментариев со статусом '%s'."
+cm_info_udt_cmts_lst = u"Пожалуйста, обновите список комментариев."
+cm_info_del_cmt = u"Удаление комментария %s"
+cm_info_upld_cmt = u"Загрузка кемментария ..."
+cm_info_updt_cmt = u"Обновление комментария ..."
+cm_info_undef = u"Не определено"
+cm_info_empty = u"<пусто>"
+cm_info_cmt_contents = u"Содержимое комментария"
+cm_info_new_cmt = u"Новый комментарий"
+cm_info_edit_cmt = u"Редактирование комментария"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Статус комментария:"
+cm_pmenu_updt_for = u"Обновить комментарии?"
+cm_pmenu_downld_fail = u"Ошибка загрузки ! Продолжить?"
+cm_pmenu_del_cmt = u"Удалить коммент. ?"
+cm_pmenu_send_cmt = u"Отправить коммент. ?"
+cm_pmenu_updt_cmt = u"Обновить коммент. ?"
+cm_pmenu_comts = u"Комментарии:"
+
+# Posts main menumenu
+pt_menu_updt = u"Обновить"
+pt_menu_view = u"Просмотр/Правка"
+pt_menu_dele = u"Удалить"
+pt_menu_lstc = u"Список комментариев"
+pt_menu_cnew = u"Создать новый"
+pt_menu_offl_publ = u"Опубликовать"
+pt_menu_clos = u"Закрыть"
+pt_menu_pict = u"Сфотографировать"
+pt_menu_canc = u"Отмена"
+pt_menu_text = u"Текст"
+pt_menu_refs = u"Ссылки/изображения"
+pt_menu_lsts = u"Списки"
+pt_menu_revs = u"Версия"
+pt_menu_prvw = u"Просмотр"
+pt_menu_titl = u"Название"
+pt_menu_cont = u"Содержимое"
+pt_menu_cats = u"Рубрики"
+pt_menu_tags = u"Теги"
+pt_menu_imgs = u"Изображения"
+pt_menu_pubs = u"Опубликовать"
+pt_menu_s2tw = u"Послать в Twitter"
+pt_menu_res = u"Размер изображения"
+pt_menu_flash = u"Режим вспышки"
+pt_menu_expos = u"Экспозамер"
+pt_menu_white = u"Баланс белого"
+
+# Posts lists
+pt_list_320x240 = u"(320x240)"
+pt_list_640x480 = u"(640x480)"
+pt_list_fsh_auto = u"Авто"
+pt_list_fsh_none = u"Нет"
+pt_list_fsh_forc = u"Принудительно"
+pt_list_loc_file = u"Локальный файл"
+pt_list_url = u"URL"
+pt_list_ins_img = u"Вставить"
+pt_list_take_img = u"Сфотографировать"
+pt_list_view_img = u"Просмотр/Список"
+pt_list_rem_img = u"Удалить"
+pt_list_save_it = u"Нет, но сохранить"
+pt_list_yes_rem_pst = u"Да, только запись на сервере"
+pt_list_yes_loc_ch = u"Да, только локальные изменения"
+pt_list_yes_del_all = u"Да, удалить все"
+pt_list_new_tag = u"Создать"
+pt_list_sel_tag = u"Выбрать"
+pt_list_rem_tag = u"Удалить"
+pt_list_lst_tag = u"Список"
+pt_list_sel_cat = u"Выбрать"
+pt_list_rem_cat = u"Удалить"
+pt_list_lst_cat = u"Список"
+
+# Posts info
+pt_info_pst_pos = u"[%d/%d] Записи"
+pt_info_downld_pt = u"Загрузка заголовков записей..."
+pt_info_no_posts = u"Нет доступных записей."
+pt_info_del_post = u"Удаление записи..."
+pt_info_post_del = u"Запись удалена."
+pt_info_updt_pst_lst = u"Пожалуйста, обновите список записей."
+pt_info_downld_post = u"Загрузка записи..."
+pt_info_empty = u"<пусто>"
+pt_info_pict = u"Сфотографировать"
+pt_info_post_contents = u"Содержимое записи"
+pt_info_new_post = u"Новая запись"
+pt_info_edit_post = u"Правка записи"
+pt_info_no_pub = u"Да"
+pt_info_draft = u"Нет (черновик)"
+pt_info_no_imgs_sel = u"Нет выбранных изображений."
+pt_info_downld_img = u"Загрузка %s"
+pt_info_send_twt = u"Отправляем заголовок на Twitter"
+pt_info_twitter_updated = u"Twitter обновлен."
+
+# Posts error messages
+pt_err_cant_del_pt = u"Невозможно удалить запись."
+pt_err_cant_start_viewf = u"Невозможно открыть видоискатель."
+pt_err_cant_stop_viewf = u"Невозможно закрыть видоискатель."
+pt_err_cant_take_pic = u"Невозможно сфотографировать."
+pt_err_cant_gen_prvw = u"Невозможно создать preview."
+pt_err_cant_prvw = u"Просмотр невозможен."
+pt_err_cant_open = u"Невозможно открыть %s"
+pt_err_cant_downld = u"Невозможно загрузить %s"
+pt_err_cant_pst_cont = u"Невозможно загрузить содержимое записи"
+pt_err_unknown_ext = u"Неизвестное расширение %s"
+pt_err_cant_tiny_url = u"Невозможно создать короткий URL."
+pt_err_cant_send_twitter = u"Невозможно отправить заголовок в Twitter."
+        
+# Posts popup menu
+pt_pmenu_posts = u"Записи:"
+pt_pmenu_del_post = u"Удалить запись ?"
+pt_pmenu_res = u"Разрешение ?"
+pt_pmenu_flash = u"Вспышка ?"
+pt_pmenu_expos = u"Экспозамер ?"
+pt_pmenu_white = u"Баланс белого ?"
+pt_pmenu_img_src = u"Изображение из:"
+pt_pmenu_img_url = u"Ссылка на изображение:"
+pt_pmenu_link_url = u"URL ссылки:"
+pt_pmenu_linkyt_url = u"Youtube URL:"
+pt_pmenu_linkqik_url = u"Qik URL:"
+pt_pmenu_send_post = u"Опубликовать запись ?"
+pt_pmenu_post_title = u"Заголовок записи:"
+pt_pmenu_images = u"Изображения"
+pt_pmenu_scale_img = u"Масштабировать изобр.?"
+pt_pmenu_scale_factor = u"Коэфф. масшт. (%)"
+pt_pmenu_tags = u"Теги:"
+pt_pmenu_cats = u"Рубрики:"
+
+# Settings info
+st_info_blog_set = u"Настройки блога"
+st_info_proxy_set = u"Настройки прокси"
+st_info_twitter_set = u"Настройки Twitter"
+st_info_blog_acc_set = u"Настройки учетной записи блога"
+        
+# Settings error
+st_err_no_access_point = u"Нет точек доступа."
+st_err_one_ap_req = u"Требуется хотя бы одна точка доступа."
+        
+# Settings query
+st_query_access_points = u"Точки доступа:"
+st_query_proxy_add = u"Адрес прокси:"
+st_query_proxy_prt = u"Порт прокси:"
+st_query_proxy_usr = u"Логин прокси:"
+st_query_proxy_pwd = u"Пароль прокси:"
+st_query_twitter_usr = u"Имя twitter:"
+st_query_twitter_pwd = u"Пароль twitter:"
+        
+# Settings menu
+st_menu_canc = u"Отмена"
+st_menu_blog = u"Учетные записи блогов"
+st_menu_proxy = u"Прокси"
+st_menu_access_point = u"Точка доступа"
+st_menu_lang = u"Язык"
+st_menu_twitter = u"Twitter"
+st_menu_blog_acc = u"Имя учетной записи:"
+st_menu_blog_url = u"URL блога:"
+st_menu_blog_usr = u"Логин:"
+st_menu_blog_pwd = u"Пароль:"
+st_menu_blog_npt = u"Количество записей:"
+st_menu_blog_cpp = u"Количество комментариев на запись:"
+st_menu_blog_key = u"Ключ API:"
+st_menu_proxy_ena = u"Включено"
+st_menu_proxy_add = u"Адрес"
+st_menu_proxy_prt = u"Порт"
+st_menu_proxy_usr = u"Логин"
+st_menu_proxy_pwd = u"Пароль"
+st_menu_proxy_on = u"Включено"
+st_menu_proxy_off = u"Отключено"
+st_menu_twitter_ena = u"Отправить в twitter"
+st_menu_twitter_on = u"Да"
+st_menu_twitter_off = u"Нет"
+st_menu_new_blog = u"Новый"
+st_menu_cpy_blog = u"Копировать"
+st_menu_del_blog = u"Удалить"
+st_menu_edt_blog = u"Правка"
+            
+# languages support
+st_menu_en_us = u"English (USA)"
+st_menu_pt_br = u"Portuguese (Brazil)"
+st_menu_es = u"Spanish"
+st_menu_tr = u"Turkish"
+st_menu_it = u"Italian"
+st_menu_nl = u"Dutch"
+st_menu_de = u"German"
+st_menu_ro = u"Romanian"
+st_menu_zh_cn = u"Simplified Chinese"
+st_menu_fr = u"French"
+st_menu_ru = u"Russian"
+
+# Setting popup menu
+st_pmenu_lang = u"Язык:"
+
+# Window
+wi_info_exit = u"Выход"
+
+# Wpwrapper error
+wp_err_cant_del_cat = u"Невозможно удалить рубрику %s."
+wp_err_cant_create_cat = u"Невозможно создать рубрику %s."
+wp_err_cant_downl_cat =u"Невозможно получить список рубрик."
+wp_err_cant_updt_post = u"Невозможно обновить записи."
+wp_err_cant_downl_post = u"Невозможно загрузить запись. Попробуйте еще раз."
+wp_err_cant_upld_img = u"Невозможно отправить %s. Попробуйте еще раз."
+wp_err_cant_pub_post = u"Невозможно опубликовать запись. Попробуйте еще раз."
+wp_err_cant_updt_post_list = u"Невозможно обновить заголовки записей. Попробуйте еще раз."
+wp_err_cant_updt_the_post = u"Невозможно обновить запись. Попробуйте еще раз."
+wp_err_cant_downl_cmt = u"Невозможно загрузить комментарии. Попробуйте еще раз."
+wp_err_cant_updt_cmt = u"Невозможно обновить комментарии. Попробуйте еще раз."
+wp_err_cant_updt_cmt_list = u"Невозможно обновить список комментариев. Попробуйте еще раз."
+wp_err_cant_pub_cmt = u"Невозможно опубликовать комментарий. Попробуйте еще раз."
+wp_err_cant_appr_cmt = u"Невозможно одобрить комментарий. Попробуйте еще раз."
+wp_err_cant_del_cmt = u"Невозможно удалить комментарий. Попробуйте еще раз."
+
+# Wpwrapper info
+wp_info_cat_del = u"Рубрика %s удалена."
+wp_info_upld_img = u"Загружается %s..."
+wp_info_upld_post_cont = u"Отправляется содержимое записи..."
+wp_info_updt_post_list = u"Обновляется список комментариев..."
+wp_info_cmt_approved = u"Комментарий одобрен."
+wp_info_cmt_del = u"Комментарий удален."
+        
+# Wpwrapper list
+wp_list_uncategorized = u"Без рубрики"
+
+# Tags main menu
+tg_menu_updt = u"Обновить"
+tg_menu_clos = u"Закрыть"
+
+# Tags info messages
+tg_info_tag_pos = u"[%d/%d] Теги"
+tg_info_downld_tags = u"Загружаются теги..."
+tg_info_udt_tags_lst = u"Пожалуйста, обновите список тегов."
+
+# Tags error messages        
+tg_err_cant_updt_tags = u"Невозможно обновить теги."
+
+# Tags popup menu
+tg_pmenu_cats = u"Теги:"
+
+# WPStats main menu
+ws_menu_updt = u"Обновить"
+ws_menu_views = u"Просмотры"
+ws_menu_daily = u"За день"
+ws_menu_weekly = u"За неделю"
+ws_menu_monthly = u"За месяц"
+ws_menu_exit = u"Выход"
+
+# WPStats popup menu
+ws_pmenu_downl_stats = u"Загрузить статистику ?"
+
+# WPStats info messages
+ws_info_downloading = u"Загрузка статистики..."
+ws_info_downl_failed = u"Невозможно загрузить статистику. Попробуйте еще раз"
+ws_info_processing = u"Обработка..."
+# do not translate "%s\n%d", only translate views
+ws_info_views = u"%s\n%d просмотров"
+ws_info_daily = u"Просмотров за день"
+ws_info_weekly = u"Просмотров за неделю"
+ws_info_monthly = u"Просмоторов за месяц"
