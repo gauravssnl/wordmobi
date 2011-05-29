8d69479a0edf41c49355669010678217c21e45eb
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2011-05-29 17:34:35 +0000

    Indonesian language support added

---
+++ tags/wordmobi/0.9.5/src/loc/wmlocale_pt_br.py
@@ -0,0 +1,356 @@
+# -*- coding: cp1252 -*-
+
+# Translation by:
+#
+# Marcelo Barros <marcelobarrosalmeida (at) gmail.com>
+
+# Promo phrase
+promo_phrase = u"<br><br>Via <a href=\"http://wordmobi.googlecode.com\">Wordmobi</a>" 
+
+# Wordmobi main menu
+wm_menu_post = u"Posts"
+wm_menu_comm = u"Comentários"
+wm_menu_cats = u"Categorias"
+wm_menu_tags = u"Tags"
+wm_menu_sets = u"Configurações"
+wm_menu_upgr = u"Atualização"
+wm_menu_abou = u"Sobre"
+wm_menu_exit = u"Sair"
+wm_menu_stat = u"Estatísticas"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Ainda não suportado."
+wm_err_upd_page = u"Impossível acesssar a página de update %s"
+wm_err_downld_fail = u"Impossível baixar %s"
+wm_err_upd_info = u"Faltando informação sobre a atualização."
+wm_err_cache_cleanup = u"Nem todos os arquivos em %s puderam ser removidos. Tente removê-los mais tarde."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Verificando página de updates..."
+wm_info_ver_is_updt = u"Sua versão já está atualizada."
+wm_info_downloading = u"Baixando ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s salvo em %supdates. Por favor, instale-a."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Baixar %s ?"
+wm_pmenu_exit = u"Sair ?"
+
+# General messages
+gm_yes = u"Sim"
+gm_no = u"Não"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"Um cliente para Wordpress"
+ab_menu_wordmobi_auth = u"Autor"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Código fonte"
+ab_menu_wordmobi_blog = u"Blog"
+ab_menu_wordmobi_lics = u"Licença"
+ab_menu_wordmobi_warn = u"Advertência"
+ab_menu_wordmobi_warn_val = u"Assuma todos os riscos ao usar"
+
+# Categories main menu
+ca_menu_updt = u"Atualizar"
+ca_menu_dele = u"Apagar"
+ca_menu_cnew = u"Criar nova"
+ca_menu_clos = u"Fechar"
+
+# Categories popup menu
+ca_pmenu_cats = u"Categorias:"
+ca_pmenu_delete = u"Apagar %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Categorias"
+ca_info_downld_cats = u"Baixando categorias..."
+ca_info_del_cat = u"Apagando categoria %s ..."
+ca_info_create_cat = u"Criando categoria %s ..."
+
+# categories queries
+ca_query_cat_name = u"Nome da categoria:"
+
+# Comments main menu
+cm_menu_updt = u"Atualizar"
+cm_menu_view = u"Ver/Editar"
+cm_menu_dele = u"Apagar"
+cm_menu_cnew = u"Criar novo/Responder"
+cm_menu_clos = u"Fechar"
+cm_menu_canc = u"Cancelar"
+cm_menu_pstt = u"Título do post"
+cm_menu_cont = u"Conteúdo"
+cm_menu_name = u"Nome"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Website"
+
+# Comments lists
+cm_list_any = u"Qualquer"
+cm_list_spam = u"Spam"
+cm_list_moderated = u"Moderado"
+cm_list_unmoderated = u"Não moderado"
+cm_list_approve = u"Aprovar"
+cm_list_one_post = u"Um post"
+cm_list_all_posts = u"Todos os posts"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Comentários"
+cm_info_aprv_cmt = u"Aprovando comentário %s"
+cm_info_downld_pt = u"Baixando títulos dos posts..."
+cm_info_which_post = u"Para qual post?"
+cm_info_downld_cmts = u"[%d/%d] Baixando comentários ..."
+cm_info_downld_cmt = u"Baixando comentários ..."
+cm_info_no_cmts_st = u"Sem comentários com status '%s'."
+cm_info_udt_cmts_lst = u"Por favor, atualize a lista de comentários."
+cm_info_del_cmt = u"Apagando comentário %s"
+cm_info_upld_cmt = u"Enviando comentário ..."
+cm_info_updt_cmt = u"Atualizando comentário ..."
+cm_info_undef = u"Indefinido"
+cm_info_empty = u"<vazio>"
+cm_info_cmt_contents = u"Conteúdo do comentário"
+cm_info_new_cmt = u"Novo Comentário"
+cm_info_edit_cmt = u"Editar Comentário"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Status do comentário:"
+cm_pmenu_updt_for = u"Atualiz. coment. para?"
+cm_pmenu_downld_fail = u"Downl. falhou ! Continuar?"
+cm_pmenu_del_cmt = u"Apagar comentário ?"
+cm_pmenu_send_cmt = u"Enviar comentário ?"
+cm_pmenu_updt_cmt = u"Atualizar comentário ?"
+cm_pmenu_comts = u"Comentários:"
+
+# Posts main menu
+pt_menu_updt = u"Atualizar"
+pt_menu_view = u"Ver/Editar"
+pt_menu_dele = u"Apagar"
+pt_menu_lstc = u"Listar Comentários"
+pt_menu_cnew = u"Criar novo"
+pt_menu_offl_publ = u"Publicar"
+pt_menu_clos = u"Fechar"
+pt_menu_pict = u"Tirar uma foto"
+pt_menu_canc = u"Cancelar"
+pt_menu_text = u"Texto"
+pt_menu_refs = u"Links/imagens"
+pt_menu_lsts = u"Listas"
+pt_menu_revs = u"Revisão"
+pt_menu_prvw = u"Pré-visualizar"
+pt_menu_titl = u"Título"
+pt_menu_cont = u"Conteúdo"
+pt_menu_cats = u"Categorias"
+pt_menu_tags = u"Tags"
+pt_menu_imgs = u"Imagens"
+pt_menu_pubs = u"Publicar"
+pt_menu_s2tw = u"Enviar para o Twitter"
+pt_menu_res = u"Tamanho da imagem"
+pt_menu_flash = u"Modo de flash"
+pt_menu_expos = u"Modo de exposição"
+pt_menu_white = u"Equilíbrio de branco"  
+
+# Posts lists
+pt_list_320x240 = u"(320x240)"
+pt_list_640x480 = u"(640x480)"
+pt_list_fsh_auto = u"Automático"
+pt_list_fsh_none = u"Sem flash"
+pt_list_fsh_forc = u"Ligado"
+pt_list_loc_file = u"Arquivo local"
+pt_list_url = u"URL"
+pt_list_ins_img = u"Inserir"
+pt_list_take_img = u"Tirar um foto"
+pt_list_view_img = u"Ver/Listar"
+pt_list_rem_img = u"Remover"
+pt_list_save_it = u"Não, mas salve-o"
+pt_list_yes_rem_pst = u"Sim, só o post"
+pt_list_yes_loc_ch = u"Sim, só mudanças locais"
+pt_list_yes_del_all = u"Sim, tudo"
+pt_list_new_tag = u"Criar"
+pt_list_sel_tag = u"Selecionar"
+pt_list_rem_tag = u"Remover"
+pt_list_lst_tag = u"Listar"
+pt_list_sel_cat = u"Selecionar"
+pt_list_rem_cat = u"Remover"
+pt_list_lst_cat = u"Listar"
+
+# Posts info
+pt_info_pst_pos = u"[%d/%d] Posts"
+pt_info_downld_pt = u"Baixando título dos posts..."
+pt_info_no_posts = u"Sem posts disponíveis."
+pt_info_del_post = u"Apagando o post..."
+pt_info_post_del = u"Post apagado."
+pt_info_updt_pst_lst = u"Por favor, atualize a lista de posts."
+pt_info_downld_post = u"Baixando o post..."
+pt_info_empty = u"<vazio>"
+pt_info_pict = u"Tirar um Foto"
+pt_info_post_contents = u"Conteúdo do Post"
+pt_info_new_post = u"Novo Post"
+pt_info_edit_post = u"Editar Post"
+pt_info_no_pub = u"Sim"
+pt_info_draft = u"Não (rascunho)"
+pt_info_no_imgs_sel = u"Nenhuma imagem selecionada."
+pt_info_downld_img = u"Baixando %s"
+pt_info_send_twt = u"Enviando post para o Twitter"
+pt_info_twitter_updated = u"Twitter atualizado."
+
+# Posts error messages
+pt_err_cant_del_pt = u"Impossível apagar o post."
+pt_err_cant_start_viewf = u"Impossível ligar a câmera."
+pt_err_cant_stop_viewf = u"Impossível desligar a câmera."
+pt_err_cant_take_pic = u"Nenhuma foto tirada."
+pt_err_cant_gen_prvw = u"Impossível gerar o arquivo de pré-visualização."
+pt_err_cant_prvw = u"Impossível pré-visualizar."
+pt_err_cant_open = u"Impossível abrir %s"
+pt_err_cant_downld = u"Impossível baixar %s"
+pt_err_cant_pst_cont = u"Impossível baixar conteúdo do post."
+pt_err_unknown_ext = u"Extensão desconhecida para %s"
+pt_err_cant_tiny_url = u"Impossível criar URL reduzida."
+pt_err_cant_send_twitter = u"Impossível enviar post para o Twitter."
+
+# Posts popup menu
+pt_pmenu_posts = u"Posts:"
+pt_pmenu_del_post = u"Apagar post ?"
+pt_pmenu_res = u"Resolução ?"
+pt_pmenu_flash = u"Flash ?"
+pt_pmenu_expos = u"Mode de exposição ?"
+pt_pmenu_white = u"Equilíbrio de branco ?"
+pt_pmenu_img_src = u"Imagem de:"
+pt_pmenu_img_url = u"URL da imagem:"
+pt_pmenu_link_url = u"URL do link:"
+pt_pmenu_linkyt_url = u"URL do Youtube:"
+pt_pmenu_linkqik_url = u"URL do Qik:"
+pt_pmenu_send_post = u"Publicar post ?"
+pt_pmenu_post_title = u"Título do post:"
+pt_pmenu_images = u"Imagens"
+pt_pmenu_scale_img = u"Reduzir esta imagem?"
+pt_pmenu_scale_factor = u"Fator de escala (%)"
+pt_pmenu_tags = u"Tags:"
+pt_pmenu_cats = u"Categorias:"
+
+# Settings info
+st_info_blog_set = u"Config. do blog"
+st_info_proxy_set = u"Config. do proxy"
+st_info_twitter_set = u"Config. do Twitter"
+st_info_blog_acc_set = u"Config. da conta do blog"
+
+# Settings error
+st_err_no_access_point = u"Nenhum  ponto de acesso encontrado."
+st_err_one_ap_req = u"É necessário pelo menos um ponto de acesso."
+        
+# Settings query
+st_query_access_points = u"Pontos de acesso:"
+st_query_proxy_add = u"Endereço do proxy:"
+st_query_proxy_prt = u"Porta do proxy:"
+st_query_proxy_usr = u"Usuário do proxy:"
+st_query_proxy_pwd = u"Senha do proxy:"
+st_query_twitter_usr = u"Usuário do Twitter:"
+st_query_twitter_pwd = u"Senha do Twitter:"      
+
+# Settings menu
+st_menu_canc = u"Cancelar"
+st_menu_blog = u"Contas de Blog"
+st_menu_proxy = u"Proxy"
+st_menu_access_point = u"Ponto de acesso"
+st_menu_lang = u"Idioma"
+st_menu_twitter = u"Twitter"
+st_menu_blog_acc = u"Nome da conta:"
+st_menu_blog_url = u"URL do blog:"
+st_menu_blog_usr = u"Usuario:"
+st_menu_blog_pwd = u"Senha:"
+st_menu_blog_npt = u"Número de posts:"
+st_menu_blog_cpp = u"Número de comentários por post:"
+st_menu_blog_key = u"API key:"
+st_menu_proxy_ena = u"Habilitado"
+st_menu_proxy_add = u"Endereço"
+st_menu_proxy_prt = u"Porta"
+st_menu_proxy_usr = u"Usuário"
+st_menu_proxy_pwd = u"Senha"
+st_menu_proxy_on = u"Sim"
+st_menu_proxy_off = u"Não"
+st_menu_twitter_ena = u"Enviar para o Twitter"
+st_menu_twitter_on = u"Sim"
+st_menu_twitter_off = u"Não"
+st_menu_new_blog = u"Nova"
+st_menu_cpy_blog = u"Copiar"
+st_menu_del_blog = u"Apagar"
+st_menu_edt_blog = u"Editar"
+
+# languages support
+st_menu_en_us = u"Inglês (USA)"
+st_menu_pt_br = u"Português (Brasil)"
+st_menu_es = u"Espanhol"
+st_menu_tr = u"Turco"
+st_menu_it = u"Italiano"
+st_menu_nl = u"Holandês"
+st_menu_de = u"Alemão"
+st_menu_ro = u"Romeno"
+st_menu_zh_cn = u"Chinês simplificado"
+st_menu_fr = u"Francês"
+st_menu_ru = u"Russo"
+st_menu_id = u"Indonésio"
+
+# Setting popup menu
+st_pmenu_lang = u"Idioma:"
+
+# Window
+wi_info_exit = u"Sair"
+
+# Wpwrapper error
+wp_err_cant_del_cat = u"Impossível apagar a categoria %s."
+wp_err_cant_create_cat = u"Impossível criar a categoria %s."
+wp_err_cant_downl_cat =u"Impossível baixar lista de categorias."
+wp_err_cant_updt_post = u"Impossível atualizar os posts."
+wp_err_cant_downl_post = u"Impossível baixar o post. Tente de novo."
+wp_err_cant_upld_img = u"Impossível enviar %s. Tente de novo."
+wp_err_cant_pub_post = u"Impossível publicar o post. Tente de novo."
+wp_err_cant_updt_post_list = u"Impossível atualizar a lista de posts. Tente de novo."
+wp_err_cant_updt_the_post = u"Impossível atualizar o post. Tente de novo."
+wp_err_cant_downl_cmt = u"Impossível baixar os comentários. Tente de novo."
+wp_err_cant_updt_cmt = u"Impossível atualizar o comentário. Tente de novo."
+wp_err_cant_updt_cmt_list = u"Impossível atualizar a lista de comentários. Tente de novo."
+wp_err_cant_pub_cmt = u"Impossível publicar o comentário. Tente de novo."
+wp_err_cant_appr_cmt = u"Impossível aprovar o comentário. Tente de novo."
+wp_err_cant_del_cmt = u"Impossível apagar o comentário. Tente de novo."
+
+# Wpwrapper info
+wp_info_cat_del = u"Categoria %s apagada."
+wp_info_upld_img = u"Enviando %s..."
+wp_info_upld_post_cont = u"Atualizando o conteúdo do post..."
+wp_info_updt_post_list = u"Atualizando a lista de posts..."
+wp_info_cmt_approved = u"Comentário aprovado."
+wp_info_cmt_del = u"Comentário apagado."
+        
+# Wpwrapper list
+wp_list_uncategorized = u"Sem categoria"
+
+# Tags main menu
+tg_menu_updt = u"Atualizar"
+tg_menu_clos = u"Sair"
+
+# Tags info messages
+tg_info_tag_pos = u"[%d/%d] Tags"
+tg_info_downld_tags = u"Baixando tags..."
+tg_info_udt_tags_lst = u"Por favor, atualize a lista de tags."
+
+# Tags error messages        
+tg_err_cant_updt_tags = u"Impossível atualizar a lista de tags."
+
+# Tags popup menu
+tg_pmenu_cats = u"Tags:"
+
+# WPStats main menu
+ws_menu_updt = u"Atualizar"
+ws_menu_views = u"Visualização"
+ws_menu_daily = u"Diária"
+ws_menu_weekly = u"Semanal"
+ws_menu_monthly = u"Mensal"
+ws_menu_exit = u"Sair"
+
+# WPStats popup menu
+ws_pmenu_downl_stats = u"Baixar estatísticas?"
+
+# WPStats info messages
+ws_info_downloading = u"Baixando estatísticas..."
+ws_info_downl_failed = u"Impossível baixar estatísticas. Por favor, tente de novo"
+ws_info_processing = u"Processando..."
+# do not translate "%s\n%d", only translate views
+ws_info_views = u"%s\n%d visualizações"  
+ws_info_daily = u"Visualizações por dia"
+ws_info_weekly = u"Visualizações por semana"
+ws_info_monthly = u"Visualizações por mês"
