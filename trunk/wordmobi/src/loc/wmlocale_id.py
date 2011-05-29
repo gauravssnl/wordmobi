63c88953c07ba10453337b2faea5d2dbd7a44640
marcelobarrosalmeida <marcelobarrosalmeida@011f3a0a-923e-11dd-8361-953bea5d279d>
2011-05-29 16:49:31 +0000

    Indonesian translation

---
+++ trunk/wordmobi/src/loc/wmlocale_id.py
@@ -0,0 +1,355 @@
+# -*- coding: cp1252 -*-
+
+# Translation by:
+#
+# Nikko Haendratnio <nikko296 (at) gmail.com>
+# Website : http://nikko296.wordpress.com
+
+# Promo phrase 
+promo_phrase = u"<br><br>Dikirim menggunakan <a href=\"http://wordmobi.googlecode.com\">Wordmobi</a>" 
+
+# Wordmobi main menu
+wm_menu_post = u"Tulisan"
+wm_menu_comm = u"Komentar"
+wm_menu_cats = u"Kategori"
+wm_menu_tags = u"Tag"
+wm_menu_sets = u"Pengaturan"
+wm_menu_upgr = u"Upgrade"
+wm_menu_abou = u"Tentang"
+wm_menu_exit = u"Keluar"
+wm_menu_stat = u"Statistik"
+
+# Wordmobi error messages
+wm_err_not_supp = u"Belum didukung."
+wm_err_upd_page = u"Gagal mengubah halaman %s"
+wm_err_downld_fail = u"Gagal mengambil %s"
+wm_err_upd_info = u"Mengubah informasi gagal."
+wm_err_cache_cleanup = u"Tidak semua file didalam %s dapat dihapus. Coba hapus lagi nanti."
+
+# Wordmobi info messages
+wm_info_check_updt = u"Sedang mengecek halaman terbaru..."
+wm_info_ver_is_updt = u"Versi anda telah diperbarui."
+wm_info_downloading = u"Sedang mengambil ..."
+# do not translate "%supdates"
+wm_info_downld_ok = u"%s ambil di %supdates. Silahkan, instal itu."
+
+# Wordmobi popup menus
+wm_pmenu_download = "Ambil %s ?"
+wm_pmenu_exit = u"Keluar ?"
+
+# General messages
+gm_yes = u"Ya"
+gm_no = u"Tidak"
+
+# About main menu
+ab_menu_wordmobi_defi_val = u"Program Wordpress"
+ab_menu_wordmobi_auth = u"Penulis"
+ab_menu_wordmobi_mail = u"Email"
+ab_menu_wordmobi_srcc = u"Kode sumber"
+ab_menu_wordmobi_blog = u"Blog"
+ab_menu_wordmobi_lics = u"Lisensi"
+ab_menu_wordmobi_warn = u"Peringatan"
+ab_menu_wordmobi_warn_val = u"Resiko ditanggung anda"
+
+# Categories main menu
+ca_menu_updt = u"Ubah"
+ca_menu_dele = u"Hapus"
+ca_menu_cnew = u"Buat baru"
+ca_menu_clos = u"Tutup"
+
+# Categories popup menu
+ca_pmenu_cats = u"Kategori:"
+ca_pmenu_delete = u"Hapus %s ?"
+
+# Categories info messages
+ca_info_cat_pos = u"[%d/%d] Kategori"
+ca_info_downld_cats = u"Sedang mengambil kategori..."
+ca_info_del_cat = u"Sedang menghapus kategori %s ..."
+ca_info_create_cat = u"Sedang membuat kategori %s ..."
+
+# categories queries
+ca_query_cat_name = u"Nama kategori:"
+
+# Comments main menu
+cm_menu_updt = u"Ubah"
+cm_menu_view = u"Lihat/Sunting"
+cm_menu_dele = u"Hapus"
+cm_menu_cnew = u"Buat baru/Balas"
+cm_menu_clos = u"Tutup"
+cm_menu_canc = u"Batal"
+cm_menu_pstt = u"Judul tulisan"
+cm_menu_cont = u"Isi"
+cm_menu_name = u"Nama"
+cm_menu_mail = u"Email"
+cm_menu_webs = u"Situs"
+
+# Comments lists
+cm_list_any = u"Semua"
+cm_list_spam = u"Spam"
+cm_list_moderated = u"Sudah dimoderasi"
+cm_list_unmoderated = u"Belum dimoderasi"
+cm_list_approve = u"Setuju"
+cm_list_one_post = u"Satu tulisan"
+cm_list_all_posts = u"Semua tulisan"
+
+# Comments info
+cm_info_cat_pos = u"[%d/%d] Komentar"
+cm_info_aprv_cmt = u"Sedang menyetujui komentar %s"
+cm_info_downld_pt = u"Sedang mengambil judul tulisan..."
+cm_info_which_post = u"Untuk tulisan yang mana?"
+cm_info_downld_cmts = u"[%d/%d] Sedang mengambil komentar ..."
+cm_info_downld_cmt = u"Sedang mengambil komentar ..."
+cm_info_no_cmts_st = u"Tidak ada komentar dengan status '%s'."
+cm_info_udt_cmts_lst = u"Silahkan, mengubah daftar komentar."
+cm_info_del_cmt = u"Sedang menghapus komentar %s"
+cm_info_upld_cmt = u"Sedang mengunggah komentar ..."
+cm_info_updt_cmt = u"Sedang memperbarui komentar ..."
+cm_info_undef = u"Tidak ditentukan"
+cm_info_empty = u"<kosong>"
+cm_info_cmt_contents = u"Isi Komentar"
+cm_info_new_cmt = u"Komentar Baru"
+cm_info_edit_cmt = u"Sunting Komentar"
+
+# Comments popup menu
+cm_pmenu_cmt_status = u"Comment status:"
+cm_pmenu_updt_for = u"Mengubah komentar untuk?"
+cm_pmenu_downld_fail = u"Mengambil gagal ! Lanjutkan?"
+cm_pmenu_del_cmt = u"Hapus komentar ?"
+cm_pmenu_send_cmt = u"Kirim komentar ?"
+cm_pmenu_updt_cmt = u"Ubah komentar ?"
+cm_pmenu_comts = u"Komentar:"
+
+# Posts main menu
+pt_menu_updt = u"Ubah"
+pt_menu_view = u"Lihat/Sunting"
+pt_menu_dele = u"Hapus"
+pt_menu_lstc = u"Daftar Komentar"
+pt_menu_cnew = u"Buat baru"
+pt_menu_offl_publ = u"Terbitkan"
+pt_menu_clos = u"Tutup"
+pt_menu_pict = u"Ambil foto"
+pt_menu_canc = u"Batal"
+pt_menu_text = u"Teks"
+pt_menu_refs = u"Pranala/gambar"
+pt_menu_lsts = u"Daftar"
+pt_menu_revs = u"Perbaikan"
+pt_menu_prvw = u"Pratinjau"
+pt_menu_titl = u"Judul"
+pt_menu_cont = u"Isi"
+pt_menu_cats = u"Kategori"
+pt_menu_tags = u"Tag"
+pt_menu_imgs = u"Gambar"
+pt_menu_pubs = u"Terbitkan"
+pt_menu_s2tw = u"Kirim ke Twitter"
+pt_menu_res = u"Ukuran gambar"
+pt_menu_flash = u"Mode flash"
+pt_menu_expos = u"Mode exposure"
+pt_menu_white = u"Mode white bal."        
+
+# Posts lists
+pt_list_320x240 = u"(320x240)"
+pt_list_640x480 = u"(640x480)"
+pt_list_fsh_auto = u"Otomatis"
+pt_list_fsh_none = u"Tidak ada"
+pt_list_fsh_forc = u"Terpaksa"
+pt_list_loc_file = u"File lokal"
+pt_list_url = u"URL"
+pt_list_ins_img = u"Tambah"
+pt_list_take_img = u"Ambil foto"
+pt_list_view_img = u"Lihat/Daftar"
+pt_list_rem_img = u"Hapus"
+pt_list_save_it = u"Tidak, tetapi simpan ini"
+pt_list_yes_rem_pst = u"Ya, hanya tulisan disana"
+pt_list_yes_loc_ch = u"Ya, hanya perubahan lokal"
+pt_list_yes_del_all = u"Ya, hapus semua"
+pt_list_new_tag = u"Membuat"
+pt_list_sel_tag = u"Memilih"
+pt_list_rem_tag = u"Menghapus"
+pt_list_lst_tag = u"Daftar"
+pt_list_sel_cat = u"Memilih"
+pt_list_rem_cat = u"Menghapus"
+pt_list_lst_cat = u"Daftar"
+
+# Posts info
+pt_info_pst_pos = u"[%d/%d] Kiriman"
+pt_info_downld_pt = u"Sedang mengambil judul tulisan..."
+pt_info_no_posts = u"Tidak ada tulisan tersedia."
+pt_info_del_post = u"Sedang menghapus tulisan..."
+pt_info_post_del = u"Tulisan telah dihapus."
+pt_info_updt_pst_lst = u"Silahkan, mengubah daftar tulisan."
+pt_info_downld_post = u"Sedang mengambil tulisan..."
+pt_info_empty = u"<kosong>"
+pt_info_pict = u"Ambil foto"
+pt_info_post_contents = u"Isi tulisan"
+pt_info_new_post = u"Tulisan baru"
+pt_info_edit_post = u"Sunting tulisan"
+pt_info_no_pub = u"Ya"
+pt_info_draft = u"Tidak (konsep)"
+pt_info_no_imgs_sel = u"Tidak ada gambar dipilih."
+pt_info_downld_img = u"Sedang mengambil %s"
+pt_info_send_twt = u"Sedang mengirim judul tulisan ke Twitter"
+pt_info_twitter_updated = u"Twitter telah perbarui."
+
+# Posts error messages
+pt_err_cant_del_pt = u"Gagal menghapus tulisan."
+pt_err_cant_start_viewf = u"Tidak dapat memulai the view finder."
+pt_err_cant_stop_viewf = u"Tidak dapat menghentikan view finder."
+pt_err_cant_take_pic = u"Tidak dapat mengambil foto apapun."
+pt_err_cant_gen_prvw = u"Tidak dapat menghasilkan file pratinjau."
+pt_err_cant_prvw = u"Gagal pratinjau."
+pt_err_cant_open = u"Gagal membuka %s"
+pt_err_cant_downld = u"Gagal mengambil %s"
+pt_err_cant_pst_cont = u"Gagal mengambil isi tulisan"
+pt_err_unknown_ext = u"Ekstensi tidak diketahui untuk %s"
+pt_err_cant_tiny_url = u"Gagal membuat URL pendek."
+pt_err_cant_send_twitter = u"Gagal mengirim judul tulisan ke twitter."
+
+# Posts popup menu
+pt_pmenu_posts = u"Tulisan:"
+pt_pmenu_del_post = u"Hapus tulisan ?"
+pt_pmenu_res = u"Ukuran gambar ?"
+pt_pmenu_flash = u"Mode flash ?"
+pt_pmenu_expos = u"Mode exposure ?"
+pt_pmenu_white = u"Mode white bal. ?"
+pt_pmenu_img_src = u"Gambar dari:"
+pt_pmenu_img_url = u"URL Gambar:"
+pt_pmenu_link_url = u"URL Pranala:"
+pt_pmenu_linkyt_url = u"URL Youtube:"
+pt_pmenu_linkqik_url = u"URL Qik:"
+pt_pmenu_send_post = u"Terbitkan tulisan ?"
+pt_pmenu_post_title = u"Judul tulisan:"
+pt_pmenu_images = u"Gambar"
+pt_pmenu_scale_img = u"Skala gambar ini?"
+pt_pmenu_scale_factor = u"Faktor skala (%)"
+pt_pmenu_tags = u"Tag:"
+pt_pmenu_cats = u"Kategori:"
+
+# Settings info
+st_info_blog_set = u"Pengaturan blog"
+st_info_proxy_set = u"Pengaturan proxy"
+st_info_twitter_set = u"Pengaturan twitter"
+st_info_blog_acc_set = u"Pengaturan akun blog"
+
+# Settings error
+st_err_no_access_point = u"Tidak dapat mencari jalur akses apapun."
+st_err_one_ap_req = u"Setidaknya satu jalur akses diperlukan."
+
+# Settings query
+st_query_access_points = u"Jalur akses:"
+st_query_proxy_add = u"Alamat proxy:"
+st_query_proxy_prt = u"Port proxy:"
+st_query_proxy_usr = u"Username proxy:"
+st_query_proxy_pwd = u"Password proxy:"
+st_query_twitter_usr = u"Username twitter:"
+st_query_twitter_pwd = u"Password twitter:"
+
+# Settings menu
+st_menu_canc = u"Batal"
+st_menu_blog = u"Akun Blog"
+st_menu_proxy = u"Proxy"
+st_menu_access_point = u"Jalur Akses"
+st_menu_lang = u"Bahasa"
+st_menu_twitter = u"Twitter"
+st_menu_blog_acc = u"Nama akun:"
+st_menu_blog_url = u"URL Blog:"
+st_menu_blog_usr = u"Username:"
+st_menu_blog_pwd = u"Password:"
+st_menu_blog_npt = u"Jumlah tulisan:"
+st_menu_blog_cpp = u"Jumlah komentar pertulisan:"
+st_menu_blog_key = u"API key:"
+st_menu_proxy_ena = u"Diaktifkan"
+st_menu_proxy_add = u"Alamat"
+st_menu_proxy_prt = u"Port"
+st_menu_proxy_usr = u"Username"
+st_menu_proxy_pwd = u"Password"
+st_menu_proxy_on = u"Diaktifkan"
+st_menu_proxy_off = u"Tidak diaktifkan"
+st_menu_twitter_ena = u"Kirim ke twitter"
+st_menu_twitter_on = u"Ya"
+st_menu_twitter_off = u"Tidak"
+st_menu_new_blog = u"Baru"
+st_menu_cpy_blog = u"Salin"
+st_menu_del_blog = u"Hapus"
+st_menu_edt_blog = u"Sunting"
+
+# languages support
+st_menu_en_us = u"Inggris (USA)"
+st_menu_pt_br = u"Portugis (Brazil)"
+st_menu_es = u"Spanyol"
+st_menu_tr = u"Turki"
+st_menu_it = u"Itali"
+st_menu_nl = u"Belanda"
+st_menu_de = u"Jerman"
+st_menu_ro = u"Rumanian"
+st_menu_zh_cn = u"Cina"
+st_menu_fr = u"Perancis"
+st_menu_ru = u"Rusia"
+
+# Setting popup menu
+st_pmenu_lang = u"Bahasa:"
+
+# Window
+wi_info_exit = u"Keluar"
+
+# Wpwrapper error
+wp_err_cant_del_cat = u"Gagal menghapus kategori %s."
+wp_err_cant_create_cat = u"Gagal membuat kategori %s."
+wp_err_cant_downl_cat =u"Gagal menerima daftar kategori."
+wp_err_cant_updt_post = u"Gagal mengubah tulisan."
+wp_err_cant_downl_post = u"Gagal mengambil tulisan. Coba lagi."
+wp_err_cant_upld_img = u"Gagal mengunggah %s. Coba lagi."
+wp_err_cant_pub_post = u"Gagal menerbitkan tulisan. Coba lagi."
+wp_err_cant_updt_post_list = u"Gagal mengubah daftar judul tulisan. Coba lagi."
+wp_err_cant_updt_the_post = u"Gagal mengubah tulisan. Coba lagi."
+wp_err_cant_downl_cmt = u"Gagal mengambil komentar. Coba lagi."
+wp_err_cant_updt_cmt = u"Gagal mengubah komentar. Coba lagi."
+wp_err_cant_updt_cmt_list = u"Gagal mengubah daftar komentar. Coba lagi."
+wp_err_cant_pub_cmt = u"Gagal menerbitkan komentar. Coba lagi."
+wp_err_cant_appr_cmt = u"Gagal menyetujui komentar. Coba lagi."
+wp_err_cant_del_cmt = u"Gagal menghapus komentar. Coba lagi."
+
+# Wpwrapper info
+wp_info_cat_del = u"Kategori %s telah dihapus."
+wp_info_upld_img = u"Sedang mengunggah %s..."
+wp_info_upld_post_cont = u"Sedang mengunggah isi tulisan..."
+wp_info_updt_post_list = u"Sedang mengubah daftar tulisan..."
+wp_info_cmt_approved = u"Komentar disetujui."
+wp_info_cmt_del = u"Komentar dihapus."
+
+# Wpwrapper list
+wp_list_uncategorized = u"Tidak terkategori"
+
+# Tags main menu
+tg_menu_updt = u"Ubah"
+tg_menu_clos = u"Tutup"
+
+# Tags info messages
+tg_info_tag_pos = u"[%d/%d] Tag"
+tg_info_downld_tags = u"Sedang mengambil tag..."
+tg_info_udt_tags_lst = u"Silahkan, mengubah daftar tag."
+
+# Tags error messages        
+tg_err_cant_updt_tags = u"Gagal mengubah tag."
+
+# Tags popup menu
+tg_pmenu_cats = u"Tag:"
+
+# WPStats main menu
+ws_menu_updt = u"Ubah"
+ws_menu_views = u"Lihat"
+ws_menu_daily = u"Harian"
+ws_menu_weekly = u"Mingguan"
+ws_menu_monthly = u"Bulanan"
+ws_menu_exit = u"Keluar"
+
+# WPStats popup menu
+ws_pmenu_downl_stats = u"Mengambil statistik ?"
+
+# WPStats info messages
+ws_info_downloading = u"Sedang mengambil statistik..."
+ws_info_downl_failed = u"Gagal mengambil statistik. Silahkan coba lagi"
+ws_info_processing = u"Sedang proses..."
+ws_info_views = u"%s\n%d dilihat"  # date \n num_views views
+ws_info_daily = u"Per-hari blog dilihat"
+ws_info_weekly = u"Per-minggu blog dilihat"
+ws_info_monthly = u"Per-bulan blog dilihat"
